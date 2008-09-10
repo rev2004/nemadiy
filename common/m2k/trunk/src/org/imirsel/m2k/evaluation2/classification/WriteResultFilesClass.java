bda1672b65f42f6bb1277503bce0e3171d6166a2
kriswest <kriswest@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2008-09-10 23:47:22 +0000

    Refactored PrepClassificationFriedmanTestData to reflect the fact that its really just writing CSV files.

--- common/m2k/trunk/src/org/imirsel/m2k/evaluation2/classification/WriteResultFilesClass.java
+++ common/m2k/trunk/src/org/imirsel/m2k/evaluation2/classification/WriteResultFilesClass.java
@@ -9,6 +9,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -20,10 +21,10 @@ import org.imirsel.m2k.util.noMetadataException;
  *
  * @author kriswest
  */
-public class PrepClassificationFriedmanTestDataClass {
+public class WriteResultFilesClass {
 
     /**
-     * A utility class that takes an ArrayList of Signal arrays containing algorithm 
+     * A utility function that takes an ArrayList of Signal arrays containing algorithm 
      * name and performance (per class) metadata and outputs the data into a CSV 
      * file to be used to perform significance tests in Matlab or another 
      * suitable environment.
@@ -61,7 +62,7 @@ public class PrepClassificationFriedmanTestDataClass {
         String EvaluationOutput = "";
 
         String[] runNames = new String[numAlgos];
-        EvaluationOutput += "Classification fold,Class,";
+        EvaluationOutput += "Class,";
         for (int i = 0; i < numAlgos; i++) {
             try {
                 runNames[i] = sigStore.get(i)[0].getStringMetadata(Signal.PROP_ALG_NAME).replaceAll(",", " ");
@@ -78,25 +79,31 @@ public class PrepClassificationFriedmanTestDataClass {
         }
         EvaluationOutput += "\n";
         
-        for (int i = 0; i < numRuns; i++) {
-            EvaluationOutput += i + ",";
-            String[] classNames = sigStore.get(0)[i].getStringArrayMetadata(Signal.PROP_CLASSES);
-            for (int c = 0; c < classNames.length; c++) {
-                EvaluationOutput += classNames[c].replaceAll(",", " ") + ",";
+        
+        String[] classNames = sigStore.get(0)[0].getStringArrayMetadata(Signal.PROP_CLASSES);
+        for (int c = 0; c < classNames.length; c++) {
+            EvaluationOutput += classNames[c].replaceAll(",", " ") + ",";
 
-                for (int j = 0 ; j < numAlgos; j++){
-                    try{
-                        EvaluationOutput += "" + sigStore.get(j)[i].getDoubleArrayMetadata(Signal.PROP_PERF_PER_CLASS)[c];
-                    } catch (noMetadataException e) {
-                        throw new RuntimeException("prepFriedmanTestData: Required metadata not found!\n" + e);
-                    }
-                    if (j< sigStore.size()-1){
-                        EvaluationOutput += ",";
-                    }
+            for (int j = 0 ; j < numAlgos; j++){
+                double avg = 0.0;
+                for (int i = 0; i < numRuns; i++) {
+                    avg += sigStore.get(j)[i].getDoubleArrayMetadata(Signal.PROP_PERF_PER_CLASS)[c];
                 }
+                avg /= numRuns;
+
+                try{
+                    EvaluationOutput += "" + avg;
+                } catch (noMetadataException e) {
+                    throw new RuntimeException("prepFriedmanTestData: Required metadata not found!\n" + e);
+                }
+                if (j< sigStore.size()-1){
+                    EvaluationOutput += ",";
+                }
+
             }
             EvaluationOutput += "\n";
         }
+        
 
         
         
@@ -108,7 +115,7 @@ public class PrepClassificationFriedmanTestDataClass {
         }
         File testFile = null;
         try {
-            testFile = new File(outputDirectory + File.separator + evaluationName + "_results_class_fold" + outputFileExt);
+            testFile = new File(outputDirectory + File.separator + evaluationName + "_results_class" + outputFileExt);
             BufferedWriter output = new BufferedWriter(new FileWriter(testFile));
             output.write(EvaluationOutput);
             output.close();
@@ -123,14 +130,14 @@ public class PrepClassificationFriedmanTestDataClass {
     }
     
     /**
-     * A utility class that takes an ArrayList of Signal arrays containing algorithm 
-     * name and performance metadata and outputs the data into a CSV file to
-     * be used to perform significance tests in Matlab or another suitable
-     * environment.
+     * A utility function that takes an ArrayList of Signal arrays containing 
+     * algorithm name and performance metadata and outputs the data into a CSV 
+     * file to be used to perform significance tests in Matlab or another 
+     * suitable environment.
      * 
-     * @param sigStore An ArrayList of Signal arrays containing algorithm name and
-     * performance metadata. Each Signal Object represents a single fold of the
-     * experiment (thus each array should be ordered in the same way)
+     * @param sigStore An ArrayList of Signal arrays containing algorithm name 
+     * and performance metadata. Each Signal Object represents a single fold of 
+     * the experiment (thus each array should be ordered in the same way)
      * @param outputDirectory The directory to output the CSV file into.
      * @param evaluationName The name of the evaluation (used to name output file.
      * @param outputFileExt The extension to put on the output file.
@@ -204,6 +211,100 @@ public class PrepClassificationFriedmanTestDataClass {
         File testFile = null;
         try {
             testFile = new File(outputDirectory + File.separator + evaluationName + "_results_fold" + outputFileExt);
+            BufferedWriter output = new BufferedWriter(new FileWriter(testFile));
+            output.write(EvaluationOutput);
+            output.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        if (verbose) {
+            System.out.println(EvaluationOutput);
+        }
+        return testFile;
+    }
+    
+    /**
+     * A utility function that takes an ArrayList of Signal arrays containing 
+     * algorithm name and performance metadata and outputs the mean of each 
+     * systems data into a CSV file to be used As a result sumamry.
+     * 
+     * @param sigStore An ArrayList of Signal arrays containing algorithm name 
+     * and performance metadata. Each Signal Object represents a single fold of 
+     * the experiment (thus each array should be ordered in the same way)
+     * @param outputDirectory The directory to output the CSV file into.
+     * @param evaluationName The name of the evaluation (used to name output file.
+     * @param outputFileExt The extension to put on the output file.
+     * @param verbose Determines wheter the data should be dumped to the console
+     * as well.
+     * @return A File Object indicating where the output CSV file was written to.
+     */
+    public static File prepSummaryResultData(ArrayList<Signal[]> sigStore, String outputDirectory, String evaluationName, String outputFileExt, boolean verbose) {
+        //sort systems alphabetically
+        HashMap<String,Signal[]> sigArrMap = new HashMap<String,Signal[]>();
+        for (int i = 0; i < sigStore.size(); i++) {
+            try {
+                sigArrMap.put(((Signal[]) sigStore.get(i))[0].getStringMetadata(Signal.PROP_ALG_NAME), (Signal[]) sigStore.get(i));
+            } catch (noMetadataException ex) {
+                throw new RuntimeException("prepFriedmanTestData: Required metadata not found!\n" + ex);
+            }
+        }
+        String[] keys = sigArrMap.keySet().toArray(new String[sigStore.size()]);
+        Arrays.sort(keys);
+        
+        double[] means = new double[keys.length];
+        
+        sigStore.clear();
+        for (int i = 0; i < keys.length; i++) {
+            sigStore.add(sigArrMap.get(keys[i]));
+        }
+        
+        int numAlgos = sigStore.size();
+        int numRuns = sigStore.get(0).length;
+        String EvaluationOutput = "";
+        String[] runNames = new String[numAlgos];
+        for (int i = 0; i < numAlgos; i++) {
+            try {
+                runNames[i] = sigStore.get(i)[0].getStringMetadata(Signal.PROP_ALG_NAME);
+            } catch (noMetadataException e) {
+                throw new RuntimeException("prepFriedmanTestData: Required metadata not found!\n" + e);
+            }
+            if (sigStore.get(i).length != numRuns){
+                throw new RuntimeException(runNames[i] + " had a different number of results (" + sigStore.get(i).length + ") to " + runNames[0] + " (" + numRuns + ")");
+            }
+        }
+        for (int i = 0; i < numRuns; i++) {
+            for (int j = 0 ; j < numAlgos; j++){
+                try{
+                    means[j] += sigStore.get(j)[i].getDoubleMetadata(Signal.PROP_PERF);
+                } catch (noMetadataException e) {
+                    throw new RuntimeException("prepFriedmanTestData: Required metadata not found!\n" + e);
+                }
+            }
+        }
+        for (int i = 0; i < numAlgos; i++) {
+            means[i] /= numRuns;
+        }
+        
+        
+        DecimalFormat dec = new DecimalFormat();
+        dec.setMaximumFractionDigits(2);
+        EvaluationOutput += "Participant,Average Classifcation Accuracy\n";
+        for (int i = 0; i < numAlgos; i++) {
+            EvaluationOutput += runNames[i] + ",";
+            EvaluationOutput += dec.format(means[i] * 100.0);
+            EvaluationOutput += "%\n";
+        }
+        
+        File copyToDirFile = new File(outputDirectory);
+        if (!copyToDirFile.isDirectory()) {
+            if (!copyToDirFile.mkdirs()) {
+                throw new RuntimeException("Could not create the output folder.");
+            }
+        }
+        File testFile = null;
+        try {
+            testFile = new File(outputDirectory + File.separator + evaluationName + "_avg_results" + outputFileExt);
             BufferedWriter output = new BufferedWriter(new FileWriter(testFile));
             output.write(EvaluationOutput);
             output.close();
