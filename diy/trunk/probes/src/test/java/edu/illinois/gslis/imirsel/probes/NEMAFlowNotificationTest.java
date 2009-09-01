c992e5399b15f75168a62da0e2b873fe92867713
amitku <amitku@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-09-01 18:12:14 +0000

    initial commit

---
+++ diy/trunk/probes/src/test/java/edu/illinois/gslis/imirsel/probes/NEMAFlowNotificationTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package edu.illinois.gslis.imirsel.probes;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @author Amit Kumar
+ *
+ */
+public class NEMAFlowNotificationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
