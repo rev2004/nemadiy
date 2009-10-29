61ea7fe0a9bf2b4a0b22ca796260297b797b48ef
amitku <amitku@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-10-29 16:26:28 +0000

    sample spring container testing

---
+++ flowservice/trunk/src/test/java/org/imirsel/nema/dao/impl/JobDaoTest.java
@@ -0,0 +1,38 @@
+package org.imirsel.nema.dao.impl;
+
+import org.imirsel.nema.dao.JobDao;
+import org.imirsel.nema.model.Job;
+import org.junit.Test;
+
+public class JobDaoTest extends BaseDaoTestCase {
+	
+	
+	private JobDao jobDao;
+	
+	
+	@Test
+	public void testGetJob(){
+        // query with jdbc template to get expected value
+        String query = "select id from nema_job where id=1";
+        String id  = (String)this.jdbcTemplate.queryForObject(query, String.class);
+        assertNotNull("Job of id 1 was not found", id);
+        Job job = this.jobDao.get(1l);
+        assertNotNull("Job of id 1 was not found by service", job);
+	}
+	
+
+	
+	
+	
+	
+	
+	public JobDao getJobDao() {
+		return jobDao;
+	}
+
+	public void setJobDao(JobDao jobDao) {
+		this.jobDao = jobDao;
+	}
+	
+
+}
