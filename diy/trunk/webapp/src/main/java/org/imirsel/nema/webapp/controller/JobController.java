eb20d93a945dcf75b58348c3a72ed749bc137d11
gzhu1 <gzhu1@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-11-24 22:05:45 +0000

    add some code for jobaction -gzhu1

--- diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/controller/JobController.java
+++ diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/controller/JobController.java
@@ -2,7 +2,6 @@ package org.imirsel.nema.webapp.controller;
 
 import java.util.List;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,92 +18,98 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import com.hp.hpl.jena.graph.query.Rewrite;
 
-public class JobController extends MultiActionController{
+public class JobController extends MultiActionController {
 
 	private UserManager userManager = null;
-   private FlowService flowService = null;
-   
+	private FlowService flowService = null;
 
-   public FlowService getFlowService() {
-	   return flowService;
-   }
+	public FlowService getFlowService() {
+		return flowService;
+	}
 
+	public void setFlowService(FlowService flowService) {
+		this.flowService = flowService;
+	}
 
-   public void setFlowService(FlowService flowService) {
-	   this.flowService = flowService;
-   }
+	public void setUserManager(UserManager userManager) {
+		this.userManager = userManager;
+	}
 
+	public ModelAndView runjob(HttpServletRequest req, HttpServletResponse res) {
+		User user = userManager.getCurrentUser();
+		System.out.println("USER IS ====> " + user);
 
-   public void setUserManager(UserManager userManager) {
-	   this.userManager = userManager;
-   }
-	
-	
-	public ModelAndView  runjob(HttpServletRequest req, HttpServletResponse res){
-		User user=userManager.getCurrentUser();
-		System.out.println("USER IS ====> "+ user);
-		
-		if(user==null){
-			user=userManager.getUserByUsername("admin");
+		if (user == null) {
+			user = userManager.getUserByUsername("admin");
 		}
 		long userId = user.getId();
 		String email = user.getEmail();
-		String token = userId+ "-" + System.currentTimeMillis();
+		String token = userId + "-" + System.currentTimeMillis();
 		String name = req.getParameter("name");
 		String description = req.getParameter("description");
 		String _flowId = req.getParameter("flowId");
 		long flowId = Long.parseLong(_flowId);
-		System.out.println("JOB INFO IS: " + name + "  " + description + "  " );
-		Job job=flowService.executeJob(token, name,description, flowId,userId, email);
-		List<Job> jobs=flowService.getUserJobs(userId);
-		return new ModelAndView("job/jobList", Constants.JOBLIST, jobs); 
+		System.out.println("JOB INFO IS: " + name + "  " + description + "  ");
+		Job job = flowService.executeJob(token, name, description, flowId,
+				userId, email);
+		List<Job> jobs = flowService.getUserJobs(userId);
+		return new ModelAndView("job/jobList", Constants.JOBLIST, jobs);
 	}
 
-	
-	public ModelAndView  jobdetail(HttpServletRequest req, HttpServletResponse res){
+	public ModelAndView jobdetail(HttpServletRequest req,
+			HttpServletResponse res) {
 		String _jobId = req.getParameter("id");
 		long jobId = Long.parseLong(_jobId);
 		Job job = flowService.getJob(jobId);
 		System.out.println("Show Job: " + job.getName());
 		return new ModelAndView("job/jobdetail", Constants.JOB, job);
 	}
-	
-	
-	public ModelAndView  jobaction(HttpServletRequest req, HttpServletResponse res){
+
+	public ModelAndView jobaction(HttpServletRequest req,
+			HttpServletResponse res) {
 		String _jobId = req.getParameter("id");
 		long jobId = Long.parseLong(_jobId);
+
 		Job job = flowService.getJob(jobId);
-		System.out.println("HERE ACTION: " + req.getParameter("submit"));
+		String _submitType = req.getParameter("submit");
+
+		if (_submitType.equals("Abort This Job")) {
+			flowService.abortJob(job.getId());
+		} else if (_submitType.equals("Delete This Job")) {
+			flowService.deleteJob(job.getId());
+		} 
+
+		System.out.println("HERE ACTION: " + req.getParameter(_submitType));
 		return new ModelAndView("job/jobdetail", Constants.JOB, job);
 	}
-	
-	
-	public ModelAndView  deleteJob(HttpServletRequest req, HttpServletResponse res, GetJobRequest jobRequest){
+
+	public ModelAndView deleteJob(HttpServletRequest req,
+			HttpServletResponse res, GetJobRequest jobRequest) {
 		flowService.deleteJob(jobRequest.getJobId());
 		return null;
 	}
 
-
-	public ModelAndView  getJob(HttpServletRequest req, HttpServletResponse res,GetJobRequest jobRequest){
-		Job job=flowService.getJob(jobRequest.getJobId());
+	public ModelAndView getJob(HttpServletRequest req, HttpServletResponse res,
+			GetJobRequest jobRequest) {
+		Job job = flowService.getJob(jobRequest.getJobId());
 		return null;
 	}
 
-	
-	public ModelAndView getUserJobs(HttpServletRequest req, HttpServletResponse res){
-		User user=userManager.getCurrentUser();
+	public ModelAndView getUserJobs(HttpServletRequest req,
+			HttpServletResponse res) {
+		User user = userManager.getCurrentUser();
 		long userId = user.getId();
-		List<Job> jobList=flowService.getUserJobs(userId);
+		List<Job> jobList = flowService.getUserJobs(userId);
 		return null;
 	}
-	
 
-	public ModelAndView getUserNotifications(HttpServletRequest req, HttpServletResponse res){
-		User user=userManager.getCurrentUser();
+	public ModelAndView getUserNotifications(HttpServletRequest req,
+			HttpServletResponse res) {
+		User user = userManager.getCurrentUser();
 		long userId = user.getId();
-		List<Notification> notificationList=flowService.getUserNotifications(userId);
+		List<Notification> notificationList = flowService
+				.getUserNotifications(userId);
 		return null;
 	}
-
 
 }
