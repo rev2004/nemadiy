814196b35535a9a692f300a47d66fb5757fb4f55
aehmann <aehmann@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-11-30 23:52:27 +0000

    monitoring thread for external processes

---
+++ meandre/components/trunk/src/main/java/org/imirsel/nema/util/ProcessOutputReceiverClass.java
@@ -0,0 +1,56 @@
+package org.imirsel.nema.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+
+class ProcessOutputReceiverClass implements Runnable
+{
+	// ------------------------------ FIELDS ------------------------------
+
+	/**
+	 * stream to receive data from child
+	 */
+	private final InputStream is;
+	PrintStream cout;
+
+	//--------------------------- CONSTRUCTORS ---------------------------
+
+	/**
+	 * constructor
+	 *
+	 * @param is stream to receive data from child
+	 */
+	ProcessOutputReceiverClass( InputStream is, PrintStream cout )
+	{
+		this.is = is;
+		this.cout = cout;
+	}
+
+	// -------------------------- PUBLIC INSTANCE  METHODS --------------------------
+	/**
+	 * method invoked when Receiver thread started.  Reads data from child and displays in on System.out.
+	 */
+	public void run()
+	{
+		try
+		{
+			final BufferedReader br = new BufferedReader( new InputStreamReader( is ), 1048576 );
+			String line;
+			while ( ( line = br.readLine() ) != null )
+			{
+				cout.println( line );
+			}
+			br.close();
+		}
+		catch ( IOException e )
+		{
+			throw new IllegalArgumentException( "IOException receiving data from child process." );
+		}
+	}
+
+
+}
