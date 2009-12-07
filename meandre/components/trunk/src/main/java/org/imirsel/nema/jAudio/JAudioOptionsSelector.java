e606410c2971af38e9f2f25637289d62908e3c22
mertbay <mertbay@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-12-07 20:13:39 +0000

    annotations added

--- meandre/components/trunk/src/main/java/org/imirsel/nema/jAudio/JAudioOptionsSelector.java
+++ meandre/components/trunk/src/main/java/org/imirsel/nema/jAudio/JAudioOptionsSelector.java
@@ -8,6 +8,12 @@ import java.util.HashMap;
 import java.util.logging.Logger;
 import java.lang.Boolean;
 
+import org.imirsel.nema.annotations.BooleanDataType;
+import org.imirsel.nema.annotations.DoubleDataType;
+import org.imirsel.nema.annotations.IntegerDataType;
+import org.imirsel.nema.annotations.StringDataType;
+import org.imirsel.nema.renderers.DoubleRenderer;
+
 import org.imirsel.service.ArtifactManagerImpl;
 import org.meandre.annotations.Component;
 import org.meandre.annotations.ComponentInput;
@@ -55,54 +61,64 @@ import jAudioFeatureExtractor.DataTypes.RecordingInfo;
                        final static String DATA_PROPERTY_FFILE = "Features List File";
        private String fFile = "jAudioFiles/features.xml";
 
+       @IntegerDataType()
        @ComponentProperty(defaultValue="16000.0",
                        description="Analysis Sample Rate (Hz)",
                        name="SampleRate")
                        final static String DATA_PROPERTY_SR = "SampleRate";
        private double sampleRate = 16000.0;
 
+       @IntegerDataType()
        @ComponentProperty(defaultValue="512",
                        description="Window Length (Samples)",
                        name="WindowLength")
                        final static String DATA_PROPERTY_WL = "WindowLength";
        private int windowLength = 512;
 
+       @DoubleDataType(min=0,max=1,renderer=DoubleRenderer.class)
        @ComponentProperty(defaultValue="0.0",
                        description="Window Overlap Ratio [0.0-1.0)",
                        name="WindowOverlap")
                        final static String DATA_PROPERTY_WOL = "WindowOverlap";
        private double windowOverlap = 0.0;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="false",
                        description="Normalize the windows (true/false)",
                        name="Normalize")
                        final static String DATA_PROPERTY_N = "Normalize";
        private boolean normalize = false;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="false",
                        description="Save features for each window (true/false)",
                        name="SaveWindows")
                        final static String DATA_PROPERTY_SW = "SaveWindows";
        private boolean saveWindow = false;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Save overall feature vector for each file (true/false)",
                        name="SaveOverall")
                        final static String DATA_PROPERTY_SO = "SaveOverall";
        private boolean saveOverall = false;
 
+
+       @IntegerDataType()
        @ComponentProperty(defaultValue="0",
                        description="Output format (0: ACE XML, 1: ARFF)",
                        name="OutputType")
                        final static String DATA_PROPERTY_OT = "OutputType";
        private int outputType = 0;
 
+       @StringDataType()
        @ComponentProperty(defaultValue="Feature_Values_1.xml",
                        description="Feature Values Ouput File: the features",
                        name="Feature Values Output File")
                        final static String DATA_PROPERTY_FVFILE = "Feature Values Output File";
        private String fvFile = "Feature_Values_1.xml";
 
+       @StringDataType()
        @ComponentProperty(defaultValue="Feature_Definitions_1.xml",
                        description="Feature Definitions Output File: Description of Features",
                        name="Feature Definitions Output File")
@@ -112,90 +128,110 @@ import jAudioFeatureExtractor.DataTypes.RecordingInfo;
        /*
         *  The Feature On/Offs and Attribute Values!
         */
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Spectral Centroid (true/false)",
                        name="Spectral Centroid")
                        final static String DATA_PROPERTY_CENTROID = "Spectral Centroid";
        private boolean centroid = true;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Spectral Rolloff Point (true/false)",
                        name="Spectral Rolloff Point")
                        final static String DATA_PROPERTY_ROLLOFF = "Spectral Rolloff Point";
        private boolean rolloff = true;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="0.85",
                        description="Spectral Rolloff Point Cutoff (0-1)",
                        name="Spectral Rolloff Point Value")
                        final static String DATA_PROPERTY_ROLLOFFV = "Spectral Rolloff Point Value";
        private String rolloffVal = "0.85";
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Spectral Flux (true/false)",
                        name="Spectral Flux")
                        final static String DATA_PROPERTY_FLUX = "Spectral Flux";
        private boolean flux = true;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Compactness (true/false)",
                        name="Compactness")
                        final static String DATA_PROPERTY_COMPACTNESS = "Compactness";
        private boolean compactness = true;
+       
        // Spectral Variability
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Spectral Variability (true/false)",
                        name="Spectral Variability")
                        final static String DATA_PROPERTY_VARIABILITY = "Spectral Variability";
        private boolean variability = true;
+       
        // Root Mean Square
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Root Mean Square (true/false)",
                        name="Root Mean Square")
                        final static String DATA_PROPERTY_RMS = "Root Mean Square";
        private boolean rms = true;
+       
        // Fraction Of Low Energy Windows
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Fraction Of Low Energy Windows (true/false)",
                        name="Fraction Of Low Energy Windows")
                        final static String DATA_PROPERTY_FRACTION = "Fraction Of Low Energy Windows";
        private boolean fraction = true;
+       
        //Zero Crossings
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Zero Crossing Rate (true/false)",
                        name="Zero Crossings")
                        final static String DATA_PROPERTY_ZCR = "Zero Crossings";
        private boolean zcr = true;
-       //MFCC
+       
+       //MFCC       
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include MFCC's (true/false)",
                        name="MFCC")
                        final static String DATA_PROPERTY_MFCC = "MFCC";
        private boolean mfcc = true;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include MFCC Deltas (true/false)",
                        name="Derivative of MFCC")
                        final static String DATA_PROPERTY_MFCCD = "Derivative of MFCC";
        private boolean mfccd = true;
 
+       @IntegerDataType()
        @ComponentProperty(defaultValue="13",
                        description="MFCC number of coefficents",
                        name="MFCC Number of Coefficients")
                        final static String DATA_PROPERTY_MFCCV = "MFCC Number of Coefficients";
        private String mfccVal = "13";
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Method of Moments (magnitude spectral sum, mean, centroid, skewness, kurtosis) (true/false)",
                        name="Method of Moments")
                        final static String DATA_PROPERTY_MOMENTS = "Method of Moments";
        private boolean moments = true;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Strongest Beat in the Beat Histogram in BPM (true/false)",
                        name="Strongest Beat")
                        final static String DATA_PROPERTY_SB = "Strongest Beat";
        private boolean sb = true;
 
+       @BooleanDataType()
        @ComponentProperty(defaultValue="true",
                        description="Include Strength of Strongest Beat relative to other beats (true/false)",
                        name="Strength Of Strongest Beat")
