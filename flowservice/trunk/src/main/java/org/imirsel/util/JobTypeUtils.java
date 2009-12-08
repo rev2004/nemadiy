e471f853136863e75a5a20adfbb63d25acdc134a
amitku <amitku@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-12-08 17:10:40 +0000

    added type to the job bean

---
+++ flowservice/trunk/src/main/java/org/imirsel/util/JobTypeUtils.java
@@ -0,0 +1,13 @@
+package org.imirsel.util;
+
+/**List of selected job types
+ * 
+ * @author amitku
+ *
+ */
+public class JobTypeUtils {
+	String DEFAULT_TYPE ="Analysis";
+	String TYPE_1 = "Feature Extraction";
+	String TYPE_2 = "Classification";
+	String TYPE_3 = "Evaluation";
+}
