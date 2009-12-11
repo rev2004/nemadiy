7575920fca75ef169b293944b21207fb5380d6fa
mertbay <mertbay@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-12-11 19:02:36 +0000

    to test the data list file gen

---
+++ repositoryservice/trunk/src/org/imirsel/tests/TestDataListFileGenerator.java
@@ -0,0 +1,30 @@
+package org.imirsel.tests;
+
+
+import java.sql.SQLException;
+
+import org.imirsel.nema.repository.DatasetListFileGenerator;
+import org.imirsel.nema.repository.NEMAMetadataEntry;
+
+public class TestDataListFileGenerator {
+
+	private int datasetID = 5;
+	
+	private String bitRate = "";
+	
+	private String channels = "1";
+	
+	private String clip_type = "30";
+	
+	
+	private String encoding = "wav";
+	
+	
+	private String sample_rate = "22050";
+	
+	private String delim = "\t";
+	
+	
+	
+	
+}
