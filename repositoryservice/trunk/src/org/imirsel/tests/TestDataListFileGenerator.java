3a36af4829a5eb649b172ec1959b236cc3160e19
mertbay <mertbay@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-12-11 19:34:34 +0000

    test case

--- repositoryservice/trunk/src/org/imirsel/tests/TestDataListFileGenerator.java
+++ repositoryservice/trunk/src/org/imirsel/tests/TestDataListFileGenerator.java
@@ -1,30 +1,53 @@
 package org.imirsel.tests;
 
 
+import java.io.File;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Set;
 
 import org.imirsel.nema.repository.DatasetListFileGenerator;
 import org.imirsel.nema.repository.NEMAMetadataEntry;
 
 public class TestDataListFileGenerator {
 
-	private int datasetID = 5;
+	static int datasetID = 5;
 	
-	private String bitRate = "";
+	static String bitRate = "";
 	
-	private String channels = "1";
+	static String channels = "1";
 	
-	private String clip_type = "30";
-	
-	
-	private String encoding = "wav";
+	static String clip_type = "30";
 	
 	
-	private String sample_rate = "22050";
-	
-	private String delim = "\t";
+	static String encoding = "wav";
 	
 	
+	static String sample_rate = "22050";
+	
+	static String delim = "\t";
+	
+	
+	public  static void main(String[] args){
+		
+		
+		 List<File[]> traintest_split_files;
+		 File[] gt_and_featExt_files;
+		 Set<NEMAMetadataEntry>  file_encoding_constraint;
+
+		
+	    file_encoding_constraint = DatasetListFileGenerator.buildConstraints(bitRate, channels, clip_type, encoding, sample_rate);		
+
+		File  commonStorageDir = new File("/tmp");
+		try {
+			gt_and_featExt_files = DatasetListFileGenerator.writeOutGroundTruthAndExtractionListFile(datasetID, delim, commonStorageDir, file_encoding_constraint);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 	
 	
 }
