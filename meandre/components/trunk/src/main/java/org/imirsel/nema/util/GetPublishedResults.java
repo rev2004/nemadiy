cdf3eaa73dbf34faac7dcc1de7932bc6a1619d76
mertbay <mertbay@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-12-12 22:52:10 +0000

    component to get the dataset ID and push the names and  paths  of the published results and the groundtruths for the dataset

--- meandre/components/trunk/src/main/java/org/imirsel/nema/util/GetPublishedResults.java
+++ meandre/components/trunk/src/main/java/org/imirsel/nema/util/GetPublishedResults.java
@@ -30,24 +30,42 @@ import org.imirsel.nema.repository.RepositoryClientConnectionPool;
 	
 	public class GetPublishedResults implements ExecutableComponent {
 
+	@ComponentOutput(description = "integer datasetID", name = "datasetID")
+	public final static String DATA_INPUT_ID = "datasetID";
 	
 	
-	@ComponentOutput(description = "String[] that holds the file location for the feature extraction list file", name = "featExtList")
+	@ComponentOutput(description = "String[] that holds the file names of subs published for the collection", name = "Names")
 	public final static String DATA_OUTPUT_NAMES = "Names";
 	
-	@ComponentOutput(description = "String[] that holds the file location for the feature extraction list file", name = "featExtList")
-	public final static String DATA_OUTPUT_RESULTS = "PublishedResultPaths";
+	@ComponentOutput(description = "String[] that holds the file location for the results published for the collection", name = "Results")
+	public final static String DATA_OUTPUT_RESULTS = "Results";
 	
 
+	@ComponentOutput(description = "String[] that holds the file location for the groundtruth for the collection", name = "CollectionGroundtruth")
+	public final static String DATA_OUTPUT_GROUNDTRUTH = "CollectionGroundtruth";
+
 	
-	private int datasetID=5;
 	private java.io.PrintStream cout;
-	
+	private String[] names;
+	private String[] paths;
+	private File[] gt_and_featExt_files;
+	private String[] groundtruth ;
+	private Set<NEMAMetadataEntry>  file_encoding_constraint;
+	private String processWorkingDirName;
+	private File processWorkingDir;
 
-	
 	
 	public void initialize(ComponentContextProperties cc) throws  ComponentExecutionException {
-		cout = cc.getOutputConsole();			
+		cout = cc.getOutputConsole();		
+		try {
+			processWorkingDirName=ArtifactManagerImpl.getInstance()
+			.getProcessWorkingDirectory(
+					cc.getFlowExecutionInstanceID());				
+		} catch (IOException e1) {
+			throw new ComponentExecutionException(e1);
+		}
+		processWorkingDir = new File(processWorkingDirName);
+
 
 	}
 	
@@ -58,13 +76,25 @@ import org.imirsel.nema.repository.RepositoryClientConnectionPool;
 	public void execute(ComponentContext ccp)
 	throws ComponentExecutionException, ComponentContextException {
 		
-		
+        String ID = (String)ccp.getDataComponentFromInput(DATA_INPUT_ID);
+        int datasetID = Integer.parseInt(ID);
 		RepositoryClientConnectionPool pool = RepositoryClientConnectionPool.getInstance();
 		RepositoryClientImpl client = pool.getFromPool();
 		List<PublishedResult> resultList = null;
 		try{
 		   resultList = client.getPublishedResultsForDataset(datasetID);
-		   
+		   names = new String[resultList.size()];
+		   paths = new String[resultList.size()];
+		   int ctr =0;
+			for (PublishedResult thisResult:resultList ){
+				//cout.println("Train File: " + thisFile[0].getCanonicalPath());
+				//cout.println("Test File: " + thisFile[1].getCanonicalPath());
+				names[ctr]=thisResult.getResult_path();
+				paths[ctr]=thisResult.getName();
+				cout.println("Name " + ctr + ":" + names[ctr]);
+				cout.println("Path " + ctr + ":" + paths[ctr]);				
+				ctr++;
+			}	
 		   
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -74,9 +104,25 @@ import org.imirsel.nema.repository.RepositoryClientConnectionPool;
 		   pool.returnToPool(client);
 		}
 
-		
+	    file_encoding_constraint = DatasetListFileGenerator.buildConstraints("", "1", "30", "wav", "220050");		
 
+		try {
+			gt_and_featExt_files = DatasetListFileGenerator.writeOutGroundTruthAndExtractionListFile(datasetID, "\t", processWorkingDir, file_encoding_constraint);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		 try {
+				groundtruth[0] = gt_and_featExt_files[0].getCanonicalPath();
+		 } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		 }
+			
+		 ccp.pushDataComponentToOutput(DATA_OUTPUT_NAMES, names);
+		 ccp.pushDataComponentToOutput(DATA_OUTPUT_RESULTS, paths);
+		 ccp.pushDataComponentToOutput(DATA_OUTPUT_GROUNDTRUTH, groundtruth);
 		
 		
 	
