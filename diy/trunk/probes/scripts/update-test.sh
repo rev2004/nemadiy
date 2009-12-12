374228e814390483c91a047ff2178d48c9340e3d
amitku <amitku@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-12-12 06:53:19 +0000

    result directory and files are stored as absolute paths.

--- diy/trunk/probes/scripts/update-test.sh
+++ diy/trunk/probes/scripts/update-test.sh
@@ -1,2 +1,3 @@
+cd ..
 mvn deploy -Dmaven.test.skip=true
-scp target/probes-1.0.2-SNAPSHOT.jar amit@128.174.154.145:/home/amit/projects/Meandre-Infrastructure-1.4.5/nema/lib
+scp target/probes-1.0.2-SNAPSHOT.jar amit@128.174.154.145:/home/amit/projects/Meandre-Infrastructure-1.4.8/nema/lib
