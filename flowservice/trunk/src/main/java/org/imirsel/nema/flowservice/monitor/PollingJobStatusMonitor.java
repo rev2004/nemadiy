5d9aa430bc9470c20e06bec5407f8d1ad25d8d1c
shirk@illinois.edu <shirk@illinois.edu@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2009-12-18 01:22:13 +0000

    Removed old status update detector runnable.

--- flowservice/trunk/src/main/java/org/imirsel/nema/flowservice/monitor/PollingJobStatusMonitor.java
+++ flowservice/trunk/src/main/java/org/imirsel/nema/flowservice/monitor/PollingJobStatusMonitor.java
@@ -72,7 +72,7 @@ public class PollingJobStatusMonitor implements JobStatusMonitor {
          .newSingleThreadScheduledExecutor();
 
       updateDetectorFuture = executor.scheduleWithFixedDelay(
-         new StatusUpdateDetectorTest(), 5, POLL_PERIOD, TimeUnit.SECONDS);
+         new StatusUpdateDetector(), 5, POLL_PERIOD, TimeUnit.SECONDS);
    }
 
    //~ Constructors ------------------------------------------------------------
@@ -150,120 +150,6 @@ public class PollingJobStatusMonitor implements JobStatusMonitor {
    }
 
    //~ Inner Classes -----------------------------------------------------------
-
-   /**
-    * Runs each time the monitor wakes up to get updated job statuses.
-    *
-    * @author TODO: author
-    * @since TODO: version
-    * @proposedRating red TODO: author
-    * @acceptedRating red TODO: reviewer
-    */
-   private class StatusUpdateDetector implements Runnable {
-      public void run() {
-         jobsLock.lock();
-         logger.fine(
-            "Checking for job status updates for " + jobs.size() + " jobs.");
-         Session session = null;
-         JobDao jobDao = null;
-         try {
-
-            if (jobs.size() == 0) {
-               return;
-            }
-             
-            Iterator<Job> jobIterator = jobs.keySet()
-               .iterator();
-
-            jobDao = daoFactory.getJobDao();
-
-            session = jobDao.getSessionFactory()
-               .openSession();
-            jobDao.startManagedSession(session);
-
-            while (jobIterator.hasNext()) {
-               Job cachedJob = jobIterator.next();
-               Job persistedJob = null;
-               try {
-            	  logger.fine("Checking status of job " + cachedJob.getId() + ".");
-                  persistedJob = jobDao.findById(cachedJob.getId(), false);
-                  logger.fine("Job status: " + Job.JobStatus.toJobStatus(persistedJob.getStatusCode()));
-               } catch (HibernateException e) {
-                  logger.warning("Data access exception: " + e.getMessage());
-               } catch (DataAccessException e) {
-                  logger.warning("Data access exception: " + e.getMessage());
-               } catch (Exception e) {
-            	   e.printStackTrace();
-                  logger.warning(e.getMessage());
-               }
-
-               if (persistedJob == null) {
-                  return;
-               }
-
-               Integer oldStatus = cachedJob.getStatusCode();
-               Integer newStatus = persistedJob.getStatusCode();
-               if (!oldStatus.equals(newStatus)) {
-                  try {
-                     logger.fine(
-                        "Status update for job " + cachedJob.getId() +
-                        " occurred: was " + cachedJob.getJobStatus() +
-                        ", now " + persistedJob.getJobStatus() + ".");
-                     cachedJob.setStatusCode(persistedJob.getStatusCode());
-                     cachedJob.setUpdateTimestamp(
-                        persistedJob.getUpdateTimestamp());
-                     cachedJob.setSubmitTimestamp(
-                        persistedJob.getSubmitTimestamp());
-                     cachedJob.setStartTimestamp(
-                        persistedJob.getStartTimestamp());
-                     cachedJob.setEndTimestamp(persistedJob.getEndTimestamp());
-
-                     // Invoke the update handlers for this job
-                     Set<JobStatusUpdateHandler> handlers = jobs.get(cachedJob);
-                     if (handlers == null) {
-                        logger.fine(
-                           "No handlers registered for job " +
-                           cachedJob.getId() + ".");
-                        return;
-                     } else {
-                        for (JobStatusUpdateHandler handler : handlers) {
-                           logger.fine(
-                              "Dispatching a job status update for job " +
-                              cachedJob.getId() + " to handler " + handler +
-                              ".");
-
-                           // anything can go wrong in a handler, so
-                           // catch exceptions that are thrown
-                           try {
-                              handler.jobStatusUpdate(cachedJob);
-                           } catch (Exception e) {
-                        	   e.printStackTrace();
-                              logger.warning(e.getMessage());
-                           }
-                        }
-                     }
-
-                     // Stop monitoring this job if it is finished
-                     if (cachedJob.isDone()) {
-                        logger.fine("Job " + cachedJob.getId() +
-                           " has ended. Removing it from the status monitor.");
-                        jobIterator.remove();
-                     }
-                  } catch (Exception e) {
-                     e.printStackTrace();
-                  } // end try-catch
-               } // end if
-            } // end while
-         } catch (Exception e){
-        	 e.printStackTrace();
-         }finally {
-            jobsLock.unlock();
-            jobDao.endManagedSession();
-            session.close();
-         } // end try-finally
-      } // end method run
-   }
-
    
    /**
     * Runs each time the monitor wakes up to get updated job statuses.
@@ -273,7 +159,7 @@ public class PollingJobStatusMonitor implements JobStatusMonitor {
     * @proposedRating red TODO: author
     * @acceptedRating red TODO: reviewer
     */
-   private class StatusUpdateDetectorTest implements Runnable {
+   private class StatusUpdateDetector implements Runnable {
       public void run() {
          jobsLock.lock();
 
