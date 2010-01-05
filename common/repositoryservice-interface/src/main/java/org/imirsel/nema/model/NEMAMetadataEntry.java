06be24d5e861c2ffb2bcac1d1897f8e5e60e08cc
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-01-05 11:54:52 +0000

    Share project "repositoryservice-interface" into "https://nemadiy.googlecode.com/svn"

---
+++ common/repositoryservice-interface/src/main/java/org/imirsel/nema/model/NEMAMetadataEntry.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.imirsel.nema.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author kriswest
+ */
+public class NEMAMetadataEntry implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private String type;
+    private String value;
+
+    public NEMAMetadataEntry(String type, String value){
+        this.type = type;
+        this.value = value;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType(){
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type, String value){
+        this.type = type;
+        this.value = value;
+    }
+
+    /**
+     * @return the value
+     */
+    public String getValue(){
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(String value){
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode(){
+        return (type + "--" + value).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if (!(object instanceof NEMAMetadataEntry)){
+            return false;
+        }
+        NEMAMetadataEntry other = (NEMAMetadataEntry)object;
+        if (type.equals(other.type) && value.equals(other.value)){
+            return true;
+        }
+        return false;
+    }
+
+}
