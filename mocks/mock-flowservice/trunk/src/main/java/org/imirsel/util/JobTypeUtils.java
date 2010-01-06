7c357febc7e24bde2c3ed7f3f75fdb4b71ad27e7
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-01-06 12:16:44 +0000

    added jobtype support

---
+++ mocks/mock-flowservice/trunk/src/main/java/org/imirsel/util/JobTypeUtils.java
@@ -0,0 +1,14 @@
+package org.imirsel.util;
+
+/**List of selected job types
+ * 
+ * @author amitku
+ *
+ */
+public class JobTypeUtils {
+	public static String DEFAULT_TYPE ="Inherits";
+	public static String TYPE_1 = "Feature Extraction";
+	public static String TYPE_2 = "Classification";
+	public static String TYPE_3 = "Evaluation";
+	public static String TYPE_4 = "Analysis";
+}
