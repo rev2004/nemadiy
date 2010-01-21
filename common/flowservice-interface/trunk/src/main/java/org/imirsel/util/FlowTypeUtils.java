0764f12bc9e857609e352729200ff701f55687a8
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-01-21 17:45:45 +0000

    updateded the FlowTypeUtils to support FlowTypeEnum

--- common/flowservice-interface/trunk/src/main/java/org/imirsel/util/FlowTypeUtils.java
+++ common/flowservice-interface/trunk/src/main/java/org/imirsel/util/FlowTypeUtils.java
@@ -1,11 +1,17 @@
 package org.imirsel.util;
 
+import org.imirsel.nema.model.Flow;
+import org.imirsel.nema.model.Flow.FlowType;
+
 /**List of selected job types
  * 
  * @author amitku
  *
  */
 public class FlowTypeUtils {
+	
+	public static FlowType DEFAULT_FLOW=Flow.FlowType.INHERITS;
+	
 	public static String DEFAULT_TYPE ="Inherits";
 	public static String TYPE_1 = "Feature Extraction";
 	public static String TYPE_2 = "Classification";
