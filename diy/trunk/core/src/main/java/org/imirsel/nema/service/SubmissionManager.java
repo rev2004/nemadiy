a2986f304cf99aef9a0bc4c85e7d01f3b56b7a84
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-01-30 21:51:32 +0000

    Complete - taskDIY returns data exceptions: JobController.getUserJobs
    ACCEPTED - task 25: Modify the FlowService to create/update template flows 
    http://code.google.com/p/nemadiy/issues/detail?id=25
    
    Work in progress

--- diy/trunk/core/src/main/java/org/imirsel/nema/service/SubmissionManager.java
+++ diy/trunk/core/src/main/java/org/imirsel/nema/service/SubmissionManager.java
@@ -6,12 +6,10 @@ import org.imirsel.nema.model.Submission;
 import org.imirsel.nema.model.User;
 
 public interface SubmissionManager {
-	
 	public void removeSubmission(long l); 
 	public Submission saveSubmission(Submission submission);
 	List<Submission> getSubmissions(User user);
 	List<Submission> getAllSubmissions();
 	public Submission getSubmission(User user, String type);
 	public Submission getSubmission(long submissionId);
-
 }
