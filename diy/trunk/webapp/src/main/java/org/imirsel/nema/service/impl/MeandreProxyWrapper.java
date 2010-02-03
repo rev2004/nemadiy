27ac5de4570d1d1f2ccc2816950bf992c57d91b4
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-02-03 21:02:32 +0000

    removed the xfire servlet

--- diy/trunk/webapp/src/main/java/org/imirsel/nema/service/impl/MeandreProxyWrapper.java
+++ diy/trunk/webapp/src/main/java/org/imirsel/nema/service/impl/MeandreProxyWrapper.java
@@ -18,30 +18,31 @@ import org.springframework.core.io.ClassPathResource;
 
 import com.hp.hpl.jena.rdf.model.Resource;
 
-/**This class wraps the MeandreProxy provided by nema-client
- * and provides mechanism to reconnect to the meandre server incase of a failure
+/**
+ * This class wraps the MeandreProxy provided by nema-client and provides
+ * mechanism to reconnect to the meandre server incase of a failure
  * 
  * @author Amit Kumar
- *
+ * 
  */
 public class MeandreProxyWrapper {
-	private static Logger log =  Logger.getLogger(MeandreProxyWrapper.class.getName());
+	private static Logger log = Logger.getLogger(MeandreProxyWrapper.class
+			.getName());
 	private MeandreProxy meandreProxy;
 	private final Semaphore available = new Semaphore(1, true);
 	boolean connected = Boolean.FALSE;
-	
-	
-	public MeandreProxyWrapper(){
-		
+
+	public MeandreProxyWrapper() {
+
 	}
-	
-	public void cleanup(){
-		if(meandreProxy!=null){
+
+	public void cleanup() {
+		if (meandreProxy != null) {
 			meandreProxy.destroy();
 		}
 	}
-	
-	public void init(){
+
+	public void init() {
 		System.out.println("Initing meandre proxy wrapper....");
 		try {
 			available.acquire();
@@ -51,112 +52,123 @@ public class MeandreProxyWrapper {
 			available.release();
 			return;
 		}
-		if(connected == Boolean.TRUE){
-			if(meandreProxy.ping()== true){
+		if (connected == Boolean.TRUE) {
+			if (meandreProxy.ping() == true) {
 				available.release();
 				return;
-			}else{
-				log.severe("Meandre Proxy seems to be connected: but Not able to ping "+ meandreProxy.getBaseURL());
+			} else {
+				log
+						.severe("Meandre Proxy seems to be connected: but Not able to ping "
+								+ meandreProxy.getBaseURL());
 			}
 		}
-		try{
-		ClassPathResource cr = new ClassPathResource("meandreproxy.properties");
-		if (cr.getInputStream() == null) {
-				log.severe("Error: cannot find meandreproxy.properties in the classpath");
-				connected= Boolean.FALSE;
-		}else{
-		Properties meandreProperties=new Properties();
-		meandreProperties.load(cr.getInputStream());
-		String host = meandreProperties.getProperty("host");
-		String _port = meandreProperties.getProperty("port");
-		String username = meandreProperties.getProperty("username");
-		String password = meandreProperties.getProperty("password");
-		System.out.println("HOST CONNECTING TO: "+ host + ":"+_port );
-		int port = Integer.parseInt(_port);
-		meandreProxy = new MeandreProxy(username,password,host,port);
-		meandreProxy.setCredentials(username, password);
-		log.setLevel(Level.WARNING);
-		meandreProxy.setLogger(log);
-		System.out.println("connecting to " + host + ":"+_port);
-		
-		if(!meandreProxy.getCallOk()){
-			log.severe("Error could not initialize MeandreProxy: callOk: "+ meandreProxy.getCallOk());
-			connected= Boolean.FALSE;
-		}else{
-			connected= Boolean.TRUE;
-		}
-		}
+		try {
+			ClassPathResource cr = new ClassPathResource(
+					"meandreproxy.properties");
+			if (cr.getInputStream() == null) {
+				log
+						.severe("Error: cannot find meandreproxy.properties in the classpath");
+				connected = Boolean.FALSE;
+			} else {
+				Properties meandreProperties = new Properties();
+				meandreProperties.load(cr.getInputStream());
+				String host = meandreProperties.getProperty("host");
+				String _port = meandreProperties.getProperty("port");
+				String username = meandreProperties.getProperty("username");
+				String password = meandreProperties.getProperty("password");
+				System.out.println("HOST CONNECTING TO: " + host + ":" + _port);
+				int port = Integer.parseInt(_port);
+				meandreProxy = new MeandreProxy(username, password, host, port);
+				meandreProxy.setCredentials(username, password);
+				log.setLevel(Level.WARNING);
+				meandreProxy.setLogger(log);
+				System.out.println("connecting to " + host + ":" + _port);
+
+				if (!meandreProxy.getCallOk()) {
+					log
+							.severe("Error could not initialize MeandreProxy: callOk: "
+									+ meandreProxy.getCallOk());
+					connected = Boolean.FALSE;
+				} else {
+					connected = Boolean.TRUE;
+				}
+			}
 		} catch (IOException e1) {
 			log.severe(e1.toString());
-			connected= Boolean.FALSE;
-		}finally{
-		available.release();
+			connected = Boolean.FALSE;
+		} finally {
+			available.release();
 		}
 	}
-	
-	public Set<Resource> getAvailableFlows() throws TransmissionException{
-		if(!connected){
+
+	public Set<Resource> getAvailableFlows() throws TransmissionException {
+		if (!connected) {
 			log.severe("meandre proxy is not connected to the meandre server");
-			throw new TransmissionException("Meandre Proxy is disconnected from the server");
+			throw new TransmissionException(
+					"Meandre Proxy is disconnected from the server");
 		}
-		QueryableRepository qp= meandreProxy.getRepository();
-		Set<Resource> resources=qp.getAvailableFlows();
+		QueryableRepository qp = meandreProxy.getRepository();
+		Set<Resource> resources = qp.getAvailableFlows();
 		return resources;
 	}
 
-	public QueryableRepository getRepository() throws TransmissionException{
-		if(!connected){
+	public QueryableRepository getRepository() throws TransmissionException {
+		if (!connected) {
 			log.severe("meandre proxy is not connected to the meandre server");
-			throw new TransmissionException("Meandre Proxy is disconnected from the server");
+			throw new TransmissionException(
+					"Meandre Proxy is disconnected from the server");
 		}
-		QueryableRepository qp= meandreProxy.getRepository();
+		QueryableRepository qp = meandreProxy.getRepository();
 		return qp;
 	}
 
-	public Map<String, FlowDescription> getAvailableFlowDescriptionsMap() throws TransmissionException{
-		if(!connected){
+	public Map<String, FlowDescription> getAvailableFlowDescriptionsMap()
+			throws TransmissionException {
+		if (!connected) {
 			log.severe("meandre proxy is not connected to the meandre server");
-			throw new TransmissionException("Meandre Proxy is disconnected from the server");
+			throw new TransmissionException(
+					"Meandre Proxy is disconnected from the server");
 		}
-		QueryableRepository qp= meandreProxy.getRepository();
-		Map<String, FlowDescription> map=qp.getAvailableFlowDescriptionsMap();
+		QueryableRepository qp = meandreProxy.getRepository();
+		Map<String, FlowDescription> map = qp.getAvailableFlowDescriptionsMap();
 		return map;
 	}
 
-	public boolean removeResource(String resourceURL) throws TransmissionException {
-		boolean result=meandreProxy.removeResource(resourceURL);
+	public boolean removeResource(String resourceURL)
+			throws TransmissionException {
+		boolean result = meandreProxy.removeResource(resourceURL);
 		return result;
 	}
 
-	public void uploadFlow(FlowDescription flow, boolean overwrite) throws TransmissionException{
+	public void uploadFlow(FlowDescription flow, boolean overwrite)
+			throws TransmissionException {
 		this.meandreProxy.uploadFlow(flow, overwrite);
 	}
 
-	public FlowDescription retrieveFlowDescriptor(String flowURL) throws TransmissionException{
+	public FlowDescription retrieveFlowDescriptor(String flowURL)
+			throws TransmissionException {
 		return this.meandreProxy.retrieveFlowDescriptor(flowURL);
 	}
 
-	public Set<URI> retrieveFlowUris() throws TransmissionException{
+	public Set<URI> retrieveFlowUris() throws TransmissionException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public ExecutableComponentDescription retrieveComponentDescriptor(
-			String componentURL) throws TransmissionException{
+			String componentURL) throws TransmissionException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Set<URI> retrieveComponentUris() throws TransmissionException{
+	public Set<URI> retrieveComponentUris() throws TransmissionException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public String getConsole(String uri) {
 		return this.meandreProxy.getJobConsole(uri);
-		
+
 	}
-	
-	
 
 }
