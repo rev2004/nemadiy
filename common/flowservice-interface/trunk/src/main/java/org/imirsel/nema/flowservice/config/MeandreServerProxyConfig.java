a5c3905156dad787820ba7de730a1c1a411a70e9
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-02-04 20:33:46 +0000

    interface/implementation changes

---
+++ common/flowservice-interface/trunk/src/main/java/org/imirsel/nema/flowservice/config/MeandreServerProxyConfig.java
@@ -0,0 +1,13 @@
+package org.imirsel.nema.flowservice.config;
+
+public interface MeandreServerProxyConfig {
+
+	public abstract String getHost();
+
+	public abstract int getPort();
+
+	public abstract int getMaxConcurrentJobs();
+
+	public abstract String getUsername();
+
+}
