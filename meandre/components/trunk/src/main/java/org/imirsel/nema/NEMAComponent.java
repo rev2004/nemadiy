5d338959ab444c2785206b31e676f87639b2d778
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-02-09 19:04:08 +0000

    brought code upto specs http://code.google.com/p/nemadiy/wiki/Documentation_Standards

--- meandre/components/trunk/src/main/java/org/imirsel/nema/NEMAComponent.java
+++ meandre/components/trunk/src/main/java/org/imirsel/nema/NEMAComponent.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.StreamHandler;
 
-import org.meandre.annotations.Component;
 import org.meandre.core.ComponentContext;
 import org.meandre.core.ComponentContextException;
 import org.meandre.core.ComponentContextProperties;
@@ -15,8 +14,9 @@ import org.meandre.core.ComponentExecutionException;
 import org.meandre.core.ExecutableComponent;
 
 /**
+ * This abstract class is extended by all the Nema Components.
  * @author kriswest
- *
+ * @since 0.5.0
  */
 public abstract class NEMAComponent implements ExecutableComponent {
 
@@ -24,17 +24,36 @@ public abstract class NEMAComponent implements ExecutableComponent {
 	protected Logger _logger;
 	private java.io.PrintStream cout;
 	
-	public void dispose(ComponentContextProperties ccp) 
+	/**
+	 * Marks the cout as null after flushing and releases the resources.
+	 * This method is called by the components extending this class.
+	 * 
+	 * @param componentContextProperties The component context properties of
+	 * component.
+	 * @throws ComponentContextException {@link ComponentContextException}
+	 */
+	public void dispose(ComponentContextProperties componentContextProperties) 
 			throws ComponentContextException {
+		cout.flush();
 		cout = null;
 	}
 
-	public abstract void execute(ComponentContext cc) 
+	/**
+	 * All classes extending the NemaComponent class must implement this method.
+	 */
+	public abstract void execute(ComponentContext componentContext) 
 			throws ComponentExecutionException, ComponentContextException;
 
-	public void initialize(ComponentContextProperties ccp)
+	/**
+	 * Initializes the the cout and sets up the logger.
+	 * 
+	 * @param componentContextProperties The Component Context Property of the component.
+	 * @throws ComponentExecutionException {@link ComponentContextException}
+	 * @throws ComponentContextException {@link ComponentContextException}
+	 */
+	public void initialize(ComponentContextProperties componentContextProperties)
 			throws ComponentExecutionException, ComponentContextException {
-		cout = ccp.getOutputConsole();
+		cout = componentContextProperties.getOutputConsole();
 		_logger = Logger.getLogger(this.getClass().getName());
 		_logger.addHandler(new StreamHandler(cout, new SimpleFormatter()));
 	}
