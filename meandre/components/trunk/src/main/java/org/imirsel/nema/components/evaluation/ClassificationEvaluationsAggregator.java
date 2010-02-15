31d413374b42707e0e3fc04d860666be41f63f10
kris.west@gmail.com <kris.west@gmail.com@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-02-15 23:13:35 +0000

    Migrating ClassificationEvaluationsAggregator to new evaluator implementation and fixing logging in CLassificationEvaluator

--- meandre/components/trunk/src/main/java/org/imirsel/nema/components/evaluation/ClassificationEvaluationsAggregator.java
+++ meandre/components/trunk/src/main/java/org/imirsel/nema/components/evaluation/ClassificationEvaluationsAggregator.java
@@ -11,14 +11,22 @@
 package org.imirsel.nema.components.evaluation;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.imirsel.m2k.evaluation.DataObj;
+import org.imirsel.m2k.evaluation.TaskDescription;
 import org.imirsel.m2k.evaluation2.classification.MIREXClassificationEvalMain;
 import org.imirsel.nema.annotations.BooleanDataType;
+import org.imirsel.nema.annotations.StringDataType;
 import org.imirsel.nema.artifactservice.ArtifactManagerImpl;
+import org.imirsel.nema.components.NemaComponent;
 import org.meandre.annotations.Component;
 import org.meandre.annotations.ComponentInput;
 import org.meandre.annotations.ComponentProperty;
@@ -29,7 +37,7 @@ import org.meandre.core.ComponentExecutionException;
 import org.meandre.core.ExecutableComponent;
 /** This executable component executes an external binary using the process builder.
  *
- * @author Andreas F. Ehmann;
+ * @author Andreas F. Ehmann and Kris West;
  *
  */
 @Component(creator="Andreas F. Ehmann", description="Evaluates all Multi-fold Classification " +
@@ -37,7 +45,7 @@ import org.meandre.core.ExecutableComponent;
 		name="ClassificationEvaluationsAggregator",
 		tags="test ft please hello"
 		)
-		public class ClassificationEvaluationsAggregator implements ExecutableComponent {
+		public class ClassificationEvaluationsAggregator extends NemaComponent {
 
 
 	//@ComponentInput(description="Java File Object In", name="fileObjectIn")
@@ -52,25 +60,62 @@ import org.meandre.core.ExecutableComponent;
 	@ComponentInput(description="Ground-truth File", name="Ground-truth File")
 	final static String DATA_INPUT_3= "Ground-truth File";
 
-	@BooleanDataType(hide=true)
-	@ComponentProperty(defaultValue="false",
-			description="Flag whether to perform significance tests",
-			name="Perform Significance Tests")
-			final static String DATA_PROPERTY_PERFORMSIGTESTS = "Perform Significance Tests";
-	private boolean performSigTests = false;
+	@StringDataType(hide=true)
+	@ComponentProperty(defaultValue="",
+			description="Path and name of the hierarchy file",
+			name="Hierarchy file path")
+	final static String DATA_HEIRARCY_FILE_PATH = "Hierarchy file path";
+	
+	@StringDataType()
+	@ComponentProperty(defaultValue="genre",
+			description="Metadata type to be trained on and predicted.",
+			name="Metadata to classify")
+	final static String DATA_METADATA_PREDICTED = "Metadata to classify";
+	
+	@StringDataType()
+	@ComponentProperty(defaultValue="MIREX Genre Classification",
+			description="The name of the evaluation to be used on the result pages output.",
+			name="Task Name")
+	final static String DATA_TASK_NAME = "Task Name";
+			
+	@StringDataType()
+	@ComponentProperty(defaultValue="Classification of popular music by genre",
+			description="Task Description",
+			name="Task Description")
+	final static String DATA_TASK_DESC = "Task Description";
+			
+	@StringDataType()
+	@ComponentProperty(defaultValue="",
+			description="The name of the dataset being evaluated.",
+			name="Dataset Name")
+	final static String DATA_DATASET_NAME = "Dataset Name";
+			
+	@StringDataType()
+	@ComponentProperty(defaultValue="",
+			description="Description of the dataset used.",
+			name="Dataset Description")
+	final static String DATA_DATASET_DESC = "Dataset Description";
+	
 
+	String taskName;
+	String taskDesc;
+	String datasetName;
+	String datasetDesc;
+	String metadata;
+	File hierarchyFile;
+	
 	private String processWorkingDir;
 	private String processResultsDir;
-	private Logger logger;
-	java.io.PrintStream cout;
+
 	/** This method is invoked when the Meandre Flow is being prepared for 
 	 * getting run.
 	 *
 	 * @param ccp The properties associated to a component context
+	 * @throws ComponentContextException 
+	 * @throws ComponentExecutionException 
 	 */
-	public void initialize ( ComponentContextProperties ccp ) {
-		this.logger = ccp.getLogger();
-		cout = ccp.getOutputConsole();
+	public void initialize ( ComponentContextProperties ccp ) throws ComponentExecutionException, ComponentContextException {
+		super.initialize(ccp);
 		try {
 			processWorkingDir = ArtifactManagerImpl.getInstance(ccp.getPublicResourcesDirectory())
 					.getProcessWorkingDirectory(
@@ -78,14 +123,22 @@ import org.meandre.core.ExecutableComponent;
 			processResultsDir = ArtifactManagerImpl.getInstance(ccp.getPublicResourcesDirectory())
 			.getResultLocationForJob(ccp.getFlowExecutionInstanceID());
 		} catch (IOException e1) {
-			try {
-				throw new ComponentExecutionException(e1);
-			} catch (ComponentExecutionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			ComponentExecutionException ex = new ComponentExecutionException("IOException occured when getting working and result directories!",e1);
+			_logger.log(Level.SEVERE, "Terminating execution",ex);
+			throw ex;
 		}
-		//System.setProperty("java.awt.headless", "true");
+		
+		taskName = ccp.getProperty(DATA_TASK_NAME);
+		taskDesc = ccp.getProperty(DATA_TASK_DESC);
+		datasetName = ccp.getProperty(DATA_DATASET_NAME);
+		datasetDesc = ccp.getProperty(DATA_DATASET_DESC);
+		metadata = ccp.getProperty(DATA_METADATA_PREDICTED);
+		String hierarchyFilePath = ccp.getProperty(DATA_HEIRARCY_FILE_PATH);
+		if (!hierarchyFilePath.trim().equals("")){
+			hierarchyFile = new File(hierarchyFilePath);
+		}else{
+			hierarchyFile = null;
+		}
       
 	}
 
@@ -102,65 +155,94 @@ import org.meandre.core.ExecutableComponent;
 	public void execute(ComponentContext cc) throws ComponentExecutionException, ComponentContextException {
 		//File inFile = (File)cc.getDataComponentFromInput(DATA_INPUT_1);
 		
-		performSigTests = Boolean.valueOf(cc.getProperty(DATA_PROPERTY_PERFORMSIGTESTS));
-
 		String[] dirLists = (String[])cc.getDataComponentFromInput(DATA_INPUT_1);
 		String[] nameLists = (String[])cc.getDataComponentFromInput(DATA_INPUT_2);
 		String[] gtFileName = (String[])cc.getDataComponentFromInput(DATA_INPUT_3);
+		
 		// initialize variables for MIREXClassificationEvalMain Constructor
 		String matlabPath = "/usr/local/bin/matlab";
-		String evaluationName = "OverallPublishedResults";
-	    File gtFile = new File(gtFileName[0]);
+		File gtFile = new File(gtFileName[0]);
 	    File procResDir = new File(processResultsDir);
 	    String processResultsDirName = processResultsDir;
 		try {
 			processResultsDirName = procResDir.getCanonicalPath();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			ComponentExecutionException ex = new ComponentExecutionException("Failed to get canonical path for File: " + processResultsDirName.toString(),e);
+			_logger.log(Level.SEVERE, "Terminating execution",ex);
+			throw ex;
 		}
 	    File rootEvaluationDir = new File(processResultsDirName + File.separator + "overall");
 
-	    List<String> systemNames = new ArrayList<String>();
-        List<File> resultsDirs = new ArrayList<File>();
+	    // call constructor and evaluation method
+        TaskDescription task = new TaskDescription(-1, taskName, taskDesc, metadata, -1, datasetName, datasetDesc);
+        
+        //init evaluator
+        org.imirsel.m2k.evaluation.classification.ClassificationEvaluator eval;
+		try {
+			eval = new 
+				org.imirsel.m2k.evaluation.classification.ClassificationEvaluator(task,rootEvaluationDir,rootEvaluationDir,false,matlabPath,hierarchyFile,this._logger);
+		} catch (FileNotFoundException e) {
+			ComponentExecutionException ex = new ComponentExecutionException("FileNotFoundException occured when setting up evaluator!",e);
+			_logger.log(Level.SEVERE, "Terminating execution",ex);
+			throw ex;
+		} catch (IOException e) {
+			ComponentExecutionException ex = new ComponentExecutionException("IOException occured when setting up evaluator!",e);
+			_logger.log(Level.SEVERE, "Terminating execution",ex);
+			throw ex;
+		}
+	    
+		//read Ground-truth
+        org.imirsel.m2k.evaluation.classification.ClassificationTextFile reader = new 
+        	org.imirsel.m2k.evaluation.classification.ClassificationTextFile(this._logger,metadata);
+		try {
+			List<DataObj> gt = reader.readFile(gtFile);
+			eval.setGroundTruth(gt);
+		} catch (Exception e) {
+			ComponentExecutionException ex = new ComponentExecutionException("Exception occured when reading up ground-truth from: " + gtFile.getAbsolutePath(),e);
+			_logger.log(Level.SEVERE, "Terminating execution",ex);
+			throw ex;
+		}
+	    
+	    
         if (dirLists.length != nameLists.length) {
-        	cout.println("ERROR: List of Directories and List of Algorithm Names are Different!");
-        	cout.flush();
-        	return;
+        	ComponentExecutionException ex = new ComponentExecutionException("List of directories and list of algorithm names are different lengths!");
+			_logger.log(Level.SEVERE, "Terminating execution",ex);
+			throw ex;
         }
         for (int i=0; i<dirLists.length; i++){
-        	resultsDirs.add(new File(dirLists[i]));
-        	systemNames.add(nameLists[i]);
+        	try{
+	        	List<List<DataObj>> results = reader.readDirectory(new File(dirLists[i]),metadata);
+				for(Iterator<List<DataObj>> it= results.iterator();it.hasNext();){
+		        	eval.addResults(nameLists[i], ""+i, it.next());
+		        }
+        	} catch (Exception e) {
+    			ComponentExecutionException ex = new ComponentExecutionException("Exception occured when reading up results for system '" + nameLists[i] + " from file: " + dirLists[i],e);
+    			_logger.log(Level.SEVERE, "Terminating execution",ex);
+    			throw ex;
+    		}
         }
 
-        cout.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-        cout.println("Performing Overall Evaluation of All Published Results...");
-        cout.flush();
-        // call constructor and evaluation method
-        MIREXClassificationEvalMain evaluator = new MIREXClassificationEvalMain(performSigTests,
-                matlabPath,
-                evaluationName,
-                gtFile,
-                rootEvaluationDir,
-                null,
-                systemNames,
-                resultsDirs);
-        
-        evaluator.performEvaluation();
+      //perform evaluation
+        try {
+			Map<String,DataObj> evalResults = eval.evaluate();
+		} catch (Exception e) {
+			ComponentExecutionException ex = new ComponentExecutionException("Exception occured when performing the evaluation!",e);
+			_logger.log(Level.SEVERE, "Terminating execution",ex);
+			throw ex;
+		}
 		
-        cout.println("Evaluation Complete");
-        cout.flush();
-        cout.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		_logger.info("Evaluation Complete\n" +
+        		"Results written to: " + rootEvaluationDir.getAbsolutePath());
 	}
 
 
 	/** This method is called when the Menadre Flow execution is completed.
 	 *
 	 * @param ccp The properties associated to a component context
+	 * @throws ComponentContextException 
 	 */
-	public void dispose ( ComponentContextProperties ccp ) {
-		cout.close();
-
+	public void dispose (ComponentContextProperties ccp) throws ComponentContextException {
+		super.dispose(ccp);
 	}
 
 }
