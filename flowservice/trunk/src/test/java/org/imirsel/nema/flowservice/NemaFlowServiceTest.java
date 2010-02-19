e535eff45bed8039a59ae9c22f7fe041844bab6a
shirk993 <shirk993@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-02-19 21:00:51 +0000

    Few changes to get it working with the newest changes made by Amit

--- flowservice/trunk/src/test/java/org/imirsel/nema/flowservice/NemaFlowServiceTest.java
+++ flowservice/trunk/src/test/java/org/imirsel/nema/flowservice/NemaFlowServiceTest.java
@@ -1,6 +1,10 @@
 package org.imirsel.nema.flowservice;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Logger;
@@ -15,39 +19,50 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author shirk
  */
 public class NemaFlowServiceTest {
-	private static final Logger logger = 
-		Logger.getLogger(FlowServiceApp.class.getName());
-	
-	/**
-	 * Main method for executing the application.
-	 * 
-	 * @param args Arguments for the application.
-	 */
-	public static void main(String[] args) {
-		ApplicationContext ctx  =  null;
-	    ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-	    
-		FlowService flowService = (FlowService)ctx.getBean("flowService");
-		Set<Flow> flowSet=flowService.getFlowTemplates();
-		
-		for(Flow flow:flowSet){
-			System.out.println(flow.getType().getName());
-		}
-		/*
-		for(int i=0; i<50; i++) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			
-			flowService.executeJob(UUID.randomUUID().toString(), 
-					"Test: " + new Date().toString(), "Test job " + i, 
-					1, 1L, "shirk@uiuc.edu");
-		}
-		*/
 
-	}
+   private static final Logger logger = Logger.getLogger(FlowServiceApp.class
+         .getName());
+
+   /**
+    * Main method for executing the application.
+    * 
+    * @param args Arguments for the application.
+    */
+   public static void main(String[] args) {
+      ApplicationContext ctx = null;
+      ctx = new ClassPathXmlApplicationContext(
+            "applicationContext-flowservice-test.xml");
+
+      FlowService flowService = (FlowService) ctx.getBean("flowService");
+
+      Flow template = flowService.getFlow(2);
+
+      Flow instance = new Flow();
+      instance.setCreatorId(0L);
+      instance.setDateCreated(new Date());
+      instance.setInstanceOf(template);
+      instance.setKeyWords(template.getKeyWords());
+      instance.setName("Test instance");
+      instance.setTemplate(false);
+      instance
+            .setUrl("repository\\x0\\http___test.org_datatypetest_datatypetest1266359499405_.nt");
+      instance.setDescription("An instance for testing.");
+      instance.setType(template.getType());
+
+      long instanceId = flowService.storeFlowInstance(instance);
+
+      for (int i = 0; i < 8; i++) {
+         try {
+            Thread.sleep(100);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+
+         flowService.executeJob(UUID.randomUUID().toString(), "Test: "
+               + new Date().toString(), "Test job " + i, instanceId, 0L,
+               "shirk@uiuc.edu");
+      }
+
+   }
 
 }
