66fa8b4d891bff41ceb48ca76eafc184f2985c0a
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-02-20 00:26:51 +0000

    Creating 0.5.0 branch

---
+++ common/directoryservice/branches/0.5.0/src/main/java/org/imirsel/nema/directoryservice/PluginConstants.java
@@ -0,0 +1,8 @@
+package org.imirsel.nema.directoryservice;
+
+
+
+public class PluginConstants {
+	public static final String JOB_JNDI_SERVICE = "java:/job";
+	public static final String JOBRESULT_JNDI_SERVICE ="java:/jobresult";
+}
