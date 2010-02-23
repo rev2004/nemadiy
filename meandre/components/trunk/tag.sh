44d7a0827248c89c39b9304ca8ac044b4c3b1874
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-02-23 22:44:04 +0000

    Migration to UTF-8 explicit and EOL unix

---
+++ meandre/components/trunk/tag.sh
@@ -0,0 +1,3 @@
+release=0.5.0
+module=meandre/components
+svn copy https://nemadiy.googlecode.com/svn/$module/trunk https://nemadiy.googlecode.com/svn/$module/branches/$release -m "Creating 0.5.0 branch"
