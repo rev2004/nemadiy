466ea1283136fe391834e066918c165ff61e797a
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-02-26 20:10:39 +0000

    some testing stuff

--- repositoryservice/trunk/src/test/java/org/imirsel/nema/repository/tests/RepositoryClientImplTest.java
+++ repositoryservice/trunk/src/test/java/org/imirsel/nema/repository/tests/RepositoryClientImplTest.java
@@ -5,144 +5,129 @@ import static org.junit.Assert.*;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import org.imirsel.nema.model.NemaDataset;
 import org.imirsel.nema.repository.RepositoryClientImpl;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RepositoryClientImplTest {
 
-	private RepositoryClientImpl clientimpl; 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
+	private RepositoryClientImpl clientImpl; 
+	
+	@Before
     public void setUp() throws Exception {
-		
-//		String test_url = "gserve086.lis.illinois.edu";
-//		String DB_PASS = "reduxer101";
-//		String DB_USER = "nema_user";
-//		String test_db = "nemadatarepository_dev";
-	//	conn = new DatabaseConnector(test_url, test_db, test_username, test_password);
-		
-		clientimpl = new RepositoryClientImpl();
+		System.out.println("1");
+		clientImpl = new RepositoryClientImpl();
 	}
 	
 	
 	@After
 	public void tearDown() throws Exception {
-		clientimpl = null;
+		clientImpl = null;
 	}
 
 	
 	@Test
 	public final void testgetDatasets() {
-		
+		System.out.println("IN TEST GET DATASETS");
 		List<NemaDataset> dataset = null;
 		try {
-			dataset=clientimpl.getDatasets();
-			
+			dataset=clientImpl.getDatasets();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		assertTrue(dataset!=null);
-//		{
-//		for(Iterator<NemaDataset> it= dataset.iterator(); it.hasNext();){
-//			System.out.println(dataset.get(it.next()));
-//		}
-//		}
+		
+		for(NemaDataset nd:dataset){
+			System.out.println(nd.getName());
+		}
+		
 	}
 
 	
-
-	@Test
-	public final void testGetTrackMetadataNameMap() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataName() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataID() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetCollections() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTracksInt() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataNemaTrack() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataByIDString() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataListOfNemaTrack() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataByIDListOfString() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataByIDStringInt() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataNemaTrackInt() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataByIDListOfStringInt() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetTrackMetadataListOfNemaTrackInt() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetFileNemaTrackSetOfNemaMetadataEntry() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetCollectionVersionsNemaCollection() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetCollectionVersionsInt() {
-		fail("Not yet implemented"); // TODO
-	}
+//
+//	@Test
+//	public final void testGetTrackMetadataNameMap() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataName() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataID() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetCollections() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTracksInt() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataNemaTrack() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataByIDString() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataListOfNemaTrack() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataByIDListOfString() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataByIDStringInt() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataNemaTrackInt() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataByIDListOfStringInt() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetTrackMetadataListOfNemaTrackInt() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetFileNemaTrackSetOfNemaMetadataEntry() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetCollectionVersionsNemaCollection() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public final void testGetCollectionVersionsInt() {
+//		fail("Not yet implemented"); // TODO
+//	}
 
 }
