1cf3107a1197199ce2557f17bae4cf5366f9e067
kris.west <kris.west@gmail.com>
2010-03-07 00:07:34 +0000

    DeliminatedTextFileUtilities fix that didn't seem to go plus tweaks to a couple of tests

--- analytics/trunk/src/main/java/org/imirsel/nema/analytics/util/io/DeliminatedTextFileUtilities.java
+++ analytics/trunk/src/main/java/org/imirsel/nema/analytics/util/io/DeliminatedTextFileUtilities.java
@@ -429,7 +429,7 @@ public class DeliminatedTextFileUtilities {
         int maxRowLength = 0;
 
         try{
-            InputStream iStream = ClassLoader.getSystemResourceAsStream(classPath);
+            InputStream iStream = DeliminatedTextFileUtilities.class.getResourceAsStream(classPath);
             textBuffer = new BufferedReader(new InputStreamReader(iStream));
 
             String line = null; 
@@ -453,12 +453,16 @@ public class DeliminatedTextFileUtilities {
         }
         catch (java.io.IOException ioe)
         {
-            textBuffer.close();
+        	if (textBuffer != null){
+        		textBuffer.close();
+        	}
             throw new java.io.IOException("An IOException occured while reading file: " + classPath + "\n" + ioe);
         }
         catch (java.lang.NullPointerException npe)
         {
-            textBuffer.close();
+        	if (textBuffer != null){
+        		textBuffer.close();
+        	}
             throw new IllegalArgumentException("NullPointerException caused by classpath: " + classPath, npe);
         }
 
@@ -510,36 +514,36 @@ public class DeliminatedTextFileUtilities {
                 boolean insideSpeechMarks = false;
         	
 	            for (; i < end; i++){
-	            	System.out.println("char: " + line.charAt(i));
+	            	//System.out.println("char: " + line.charAt(i));
 	            	if(line.charAt(i) == '\"'){
 	            		encounteredSpeechMarks = true;
 	                    insideSpeechMarks = !insideSpeechMarks;
-	                    System.out.println("encountered speechmark, insideSpeechMarks=" + insideSpeechMarks);
+	                    //System.out.println("encountered speechmark, insideSpeechMarks=" + insideSpeechMarks);
 	            	}
 	            	if(!insideSpeechMarks){
 	            		matcher.region(i, end);
 		            	
 		                if (matcher.find()){
-		                	System.out.println("did find");
+		                	//System.out.println("did find");
 		                    if (encounteredSpeechMarks){
 		                    	String comp = tmp.substring(lastIdx+1, i-1);
 		                        output.add(comp);
-		                        System.out.println("added " + comp + " in speech mark block");
+		                        //System.out.println("added " + comp + " in speech mark block");
 		                        encounteredSpeechMarks = false;
 		                        lastIdx = matcher.end();
 		                        i=lastIdx-1;
-		                        System.out.println("lastIdx set to " + lastIdx);
+		                        //System.out.println("lastIdx set to " + lastIdx);
 		                    }else{
 		                    	String comp = tmp.substring(lastIdx, i);
 		                        output.add(comp);
-		                        System.out.println("added " + comp + " in NO speech mark block");
+		                        //System.out.println("added " + comp + " in NO speech mark block");
 		                        
 		                        lastIdx = matcher.end();
 		                        i=lastIdx;
-		                        System.out.println("lastIdx set to " + lastIdx);
+		                        //System.out.println("lastIdx set to " + lastIdx);
 		                    }
 		                }else{
-		                	System.out.println("didn't find");
+		                	//System.out.println("didn't find");
 		                }
 	            	}
 	            }
