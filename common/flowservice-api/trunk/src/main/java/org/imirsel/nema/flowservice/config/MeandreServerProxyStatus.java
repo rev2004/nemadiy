b0da146e4b401df0a2a24083faee70167f4ea7fb
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-03-07 18:08:55 +0000

    changed the method signature to return the server list and MeandreServerProxyStatus is now serializable

--- common/flowservice-api/trunk/src/main/java/org/imirsel/nema/flowservice/config/MeandreServerProxyStatus.java
+++ common/flowservice-api/trunk/src/main/java/org/imirsel/nema/flowservice/config/MeandreServerProxyStatus.java
@@ -1,11 +1,17 @@
 package org.imirsel.nema.flowservice.config;
 
+import java.io.Serializable;
+
 /**
  * Stores {@link MeandreServerProxy} Status
  * @author kumaramit01
  * @since 0.5.0
  */
-public class MeandreServerProxyStatus {
+public class MeandreServerProxyStatus implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2032279664228170577L;
 	private boolean isHead;
 	// number of flows that are running
 	private int numRunning;
