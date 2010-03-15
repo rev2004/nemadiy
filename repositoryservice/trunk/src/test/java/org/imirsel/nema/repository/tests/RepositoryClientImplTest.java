b5b4cb67f645f14a2c733f6a05d1624a61479982
Mamta2.Singh <Mamta2.Singh@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-03-15 18:24:54 +0000

    repositoryservice  tests

--- repositoryservice/trunk/src/test/java/org/imirsel/nema/repository/tests/RepositoryClientImplTest.java
+++ repositoryservice/trunk/src/test/java/org/imirsel/nema/repository/tests/RepositoryClientImplTest.java
@@ -3,29 +3,34 @@ package org.imirsel.nema.repository.tests;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.imirsel.nema.model.NemaCollection;
 import org.imirsel.nema.model.NemaDataset;
 import org.imirsel.nema.model.NemaFile;
 import org.imirsel.nema.model.NemaMetadataEntry;
+import org.imirsel.nema.model.NemaTrack;
 import org.imirsel.nema.model.NemaTrackList;
 import org.imirsel.nema.repository.RepositoryClientImpl;
 import org.imirsel.nema.repository.RepositoryProperties;
+import org.imirsel.nema.test.BaseManagerTestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class RepositoryClientImplTest {
+public class RepositoryClientImplTest  extends BaseManagerTestCase{
 
 	private RepositoryClientImpl clientImpl; 
 	
 	@Before
     public void setUp() throws Exception {
 		clientImpl = new RepositoryClientImpl();
-		System.out.println(RepositoryProperties.DB_LOCATOR + "=" + RepositoryProperties.getProperty(RepositoryProperties.DB_LOCATOR));
-		System.out.println(RepositoryProperties.DB_NAME + "=" + RepositoryProperties.getProperty(RepositoryProperties.DB_NAME));
+		this.getLogger().info(RepositoryProperties.DB_LOCATOR + "=" + RepositoryProperties.getProperty(RepositoryProperties.DB_LOCATOR));
+		this.getLogger().info(RepositoryProperties.DB_NAME + "=" + RepositoryProperties.getProperty(RepositoryProperties.DB_NAME));
 	}
 	
 	
@@ -47,7 +52,7 @@ public class RepositoryClientImplTest {
 		assertTrue(dataset!=null);
 		
 		for(NemaDataset nd:dataset){
-			System.out.println(nd.getName());
+			this.getLogger().info(nd.getName());
 		}
 		
 	}
@@ -59,141 +64,185 @@ public class RepositoryClientImplTest {
 		try {
 			dataset=clientImpl.getDataset(10);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		assertTrue(dataset!=null);
 		
-		  System.out.println(dataset);
-				
+		this.getLogger().info(dataset.toString());
+		
 	}
 	
 	@Test
 	public final void testGetCollections() {
 		
-		List<NemaCollection>  collectionlist = null;
+		List<NemaCollection>  collectionList = null;
 		
 		try {
-			collectionlist=clientImpl.getCollections();
+			collectionList=clientImpl.getCollections();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		assertTrue(collectionlist!=null);
+		assertTrue(collectionList!=null);
 		
-		for(NemaCollection nc:collectionlist){
-			System.out.println(nc.getName());
+		for(NemaCollection nc:collectionList){
+			this.getLogger().info(nc.getName());
 		}
 		
 	}
 	
 	@Test
-	public final void testGetDatasetSubset(){
-		NemaTrackList tracklist = null;
+	public final void testGetCollectionSubset(){
+		NemaTrackList trackList = null;
 		try {
-			tracklist=clientImpl.getDatasetSubset(10);
+			trackList=clientImpl.getCollectionSubset(10);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
-		assertTrue(tracklist!=null);
+		assertTrue(trackList!=null);
 		
-		  System.out.println(tracklist);
+		this.getLogger().info(trackList.toString());
 	}
 	
 
 	@Test
 	public final void testGetFile() throws SQLException{
 	
-		NemaFile file = null;
+		NemaFile nemaFile = null;
 		//setup constraint
 		HashSet<NemaMetadataEntry> constraint = new HashSet<NemaMetadataEntry>();
         constraint.add(new NemaMetadataEntry("encoding", "mp3"));
 	    
         //try to get file
-		file=clientImpl.getFile("b004001", constraint);
+		nemaFile=clientImpl.getFile("b004001", constraint);
 		
 		//check we got the file
-		assertTrue(file!=null);
+		assertTrue(nemaFile!=null);
 		
-		  System.out.println(file);
+		this.getLogger().info(nemaFile.toString());
 		  
-		  // quotes around ? 
-		  // line 532
-		  // change db name in query to dev db 
 	}
 	
-//	@Test
-//	public final void testGetTracks(){
-//	//return list<NemaTrack>, int	
-//		List<String> idtracklist = null;
-//		try {
-//			idtracklist=clientImpl.getTrackIDs(1);
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		assertTrue(idtracklist!=null);
-//		
-//		  System.out.println(idtracklist);
-//	}
-
-//	@Test
-//	public final void testGetTrackMetadataNameMap() {
-//		fail("Not yet implemented"); // TODO
-//	}
-
-//	@Test
-//	public final void testGetTrackMetadataName() {
-//		fail("Not yet implemented"); // TODO
-//	}
-//
-//	@Test
-//	public final void testGetTrackMetadataID() {
-//		fail("Not yet implemented"); // TODO
-//	}
+	@Test
+	public final void testGetTracks(){
+		
+		List<String> idTrackList = null;
+		try {
+			idTrackList=clientImpl.getTrackIDs(1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(idTrackList!=null);
+		
+		for(String tl:idTrackList)
+			this.getLogger().info(tl.toString());
+		
+	}
 
 
+	@Test
+	public final void testGetTrackMetadataName() {
+		String trackMetaData = null;
+		try {
+			trackMetaData=clientImpl.getTrackMetadataName(4);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(trackMetaData!=null);
+		
+		this.getLogger().info(trackMetaData);
+	}
 
-//	@Test
-//	public final void testGetTrackMetadataNemaTrack() {
-//		fail("Not yet implemented"); // TODO
-//	}
-//
-//	@Test
-//	public final void testGetTrackMetadataByIDString() {
-//		fail("Not yet implemented"); // TODO
-//	}
-//
-//	@Test
-//	public final void testGetTrackMetadataListOfNemaTrack() {
-//		fail("Not yet implemented"); // TODO
-//	}
-//
-//	@Test
-//	public final void testGetTrackMetadataByIDListOfString() {
-//		fail("Not yet implemented"); // TODO
-//	}
-//
-//	@Test
-//	public final void testGetTrackMetadataByIDStringInt() {
-//		fail("Not yet implemented"); // TODO
-//	}
-//
-//	@Test
-//	public final void testGetTrackMetadataNemaTrackInt() {
-//		fail("Not yet implemented"); // TODO
-//	}
-//
-//	@Test
-//	public final void testGetTrackMetadataByIDListOfStringInt() {
-//		fail("Not yet implemented"); // TODO
-//	}
-//
-//	@Test
-//	public final void testGetTrackMetadataListOfNemaTrackInt() {
-//		fail("Not yet implemented"); // TODO
-//	}
+	@Test
+	public final void testGetTrackMetadataID() {
+		int trackMetaDataID=0;
+		try {
+			trackMetaDataID=clientImpl.getTrackMetadataID("Key");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertTrue(trackMetaDataID != 0);
+		
+		this.getLogger().info("Metadata ID = " +trackMetaDataID);
+	}
 
+
+
+	@Test
+	public final void testGetTrackMetadataNemaTrack() {
+		
+		NemaTrack nemaTrack = new NemaTrack("00010b0e1e0854b907ff6d88aff9b78b");
+		List<NemaMetadataEntry> nemaMetaDataEntryList = null;
+		try {
+		nemaMetaDataEntryList= clientImpl.getTrackMetadata(nemaTrack);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(nemaMetaDataEntryList != null);
+		
+		for(NemaMetadataEntry nd:nemaMetaDataEntryList){
+			this.getLogger().info(nd.getValue());
+		}
+		  
+	}
+
+
+	@Test
+	public final void testGetTrackMetadataListOfNemaTrack() {
+		
+		HashMap<String,List<NemaMetadataEntry>> trackMetadata = new HashMap<String, List<NemaMetadataEntry>>();
+		List<NemaTrack> trackList = new ArrayList<NemaTrack>();
+		trackList.add(new NemaTrack("000150b65660c701e58e9d4cc97f7bdf"));
+
+		
+		try {
+			trackMetadata = (HashMap<String, List<NemaMetadataEntry>>) clientImpl.getTrackMetadata(trackList);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			assertTrue(trackMetadata != null);
+			
+			   for (Entry<String,List<NemaMetadataEntry>> entry:trackMetadata.entrySet()) {
+		    	this.getLogger().info(entry.getKey().toString());
+		    	
+		    	
+		    	for(NemaMetadataEntry nemaMetadata: entry.getValue()){
+		    	this.getLogger().info(nemaMetadata.getType() + "="+ nemaMetadata.getValue());
+		    	}
+		    	
+			   }
+		
+	}
+	
+	
+	@Test
+	public final void testGetTrackMetadataByIDListOfString() {
+
+		HashMap<String,List<NemaMetadataEntry>> trackMetadata = new HashMap<String, List<NemaMetadataEntry>>();
+		List<String> list = new ArrayList<String>();
+		list.add("m000321");
+		
+		try {
+			trackMetadata = (HashMap<String, List<NemaMetadataEntry>>) clientImpl.getTrackMetadataByID(list, 1);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			assertTrue(trackMetadata != null);
+			
+			   for (Entry<String,List<NemaMetadataEntry>> entry:trackMetadata.entrySet()) {
+		    	this.getLogger().info(entry.getKey().toString());
+		    	
+		    	
+		    	for(NemaMetadataEntry nemaMetadata: entry.getValue()){
+		    	this.getLogger().info(nemaMetadata.getType() + ":"+ nemaMetadata.getValue());
+		    	}
+		    	
+			   }
+ }	
+		
 
 }
