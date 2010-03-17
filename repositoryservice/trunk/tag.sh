f417c73c8af8c1a8eb36f6e1ec059ff54f76c094
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-03-17 20:21:23 +0000

    removed offending test -based on older repository api

---
+++ repositoryservice/trunk/tag.sh
@@ -0,0 +1,3 @@
+release=0.1.0
+module=repositoryservice
+svn copy https://nemadiy.googlecode.com/svn/$module/trunk https://nemadiy.googlecode.com/svn/$module/branches/$release -m "Creating 0.1.0 branch"
