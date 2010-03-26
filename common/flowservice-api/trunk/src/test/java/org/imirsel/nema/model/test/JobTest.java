0c238292aadf2e6d3cb18d26d16bfe2b0c305348
Mamta2.Singh <Mamta2.Singh@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-03-26 23:17:33 +0000

    updated test for nema-flowservice-api

--- common/flowservice-api/trunk/src/test/java/org/imirsel/nema/model/test/JobTest.java
+++ common/flowservice-api/trunk/src/test/java/org/imirsel/nema/model/test/JobTest.java
@@ -5,6 +5,9 @@ package org.imirsel.nema.model.test;
 
 import static org.junit.Assert.*;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 
@@ -35,24 +38,30 @@ public class JobTest {
 		jobresult = new JobResult();
 		
 		Long id = Long.valueOf(1);
+		DateFormat formatter = new SimpleDateFormat("yyyy-MMM-dd");
+        Date start_date = (Date)formatter.parse("2010-March-20"); 
+        Date end_date = (Date)formatter.parse("2010-March-25");
+        Date submit_date = (Date)formatter.parse("2010-March-24");
+        
 		job.setId(id);
 		job.setName("testName");
 		job.setDescription("test Description");
 		job.setHost("test Host");
-		job.setEndTimestamp(new Date(1095379501L));
 		job.setExecPort(1361);
 		job.setPort(8080);
 		job.setNumTries(3);
 		job.setStatusCode(1);
-		job.setStartTimestamp(new Date("3/20/2010"));
-		job.setSubmitTimestamp(new Date());
-		job.setEndTimestamp(new Date());
+		job.setStartTimestamp(new Timestamp(start_date.getTime()));
+		job.setEndTimestamp(new Timestamp(end_date.getTime()));
+		job.setSubmitTimestamp(new Timestamp(submit_date.getTime()));
 		job.setExecutionInstanceId("execution instance id");
 		job.setOwnerEmail("singh14@illinois.edu");
 		job.setOwnerId(id);
 		
+		Date created_date = (Date)formatter.parse("2010-March-25");
+		
 		flow.setCreatorId(Long.valueOf(20001));
-		flow.setDateCreated(new Date());
+		flow.setDateCreated(new Timestamp(created_date.getTime()));
 		flow.setDescription("Here is flow description");
 		flow.setId(Long.valueOf(201));
 		job.setFlow(flow);
@@ -79,71 +88,73 @@ public class JobTest {
 	 * Test method for {@link org.imirsel.nema.model.Job#getUpdateTimestamp()}.
 	 */
 	
-	@Test
-	public void testGetStatusCode() {
-	int i = job.getStatusCode();
-	assertTrue(i!=0);
-	System.out.println("test status code  "+ i);
-	}
-	
-	@Test
-	public void testSetStatusCode() {
-		
-		int i=1;
-		job.setStatusCode(i);
-		
-		int j = job.getStatusCode();
-		assertTrue(j!=0);
-		System.out.println("test status  "+ j);
-		
-		Date dt = new Date();
-		
-		dt = job.getUpdateTimestamp();
-		
-		assertTrue(dt!=null);
-	
-		System.out.println("update time stamp : " + dt);
-	}
+//	@Test
+//	public void testGetStatusCode() {
+//	int i = job.getStatusCode();
+//	assertTrue(i!=0);
+//	System.out.println("test status code  "+ i);
+//	}
+//	
+//	@Test
+//	public void testSetStatusCode() {
+//		
+//		int i=1;
+//		job.setStatusCode(i);
+//		
+//		int j = job.getStatusCode();
+//		assertTrue(j!=0);
+//		System.out.println("test status  "+ j);
+//		
+//		Date dt = new Date();
+//		
+//		dt = job.getUpdateTimestamp();
+//		
+//		assertTrue(dt!=null);
+//	
+//		System.out.println("update time stamp : " + dt);
+//	}
 
 	
 	@Test
 	public void testClone()
 	{  
-		job = job.clone();
-	   assertNotNull(job.getId());
-	   assertNotNull(job.getName());
-	   assertNotNull(job.getHost());
-	   assertNotNull(job.getDescription());
-	   assertNotNull(job.getEndTimestamp());
-	   assertNotNull(job.getExecPort());
-	   assertNotNull(job.getNumTries());
-	   assertNotNull(job.getStatusCode());
-	   assertNotNull(job.getUpdateTimestamp());
-	   assertNotNull(job.getStartTimestamp());
-	   assertNotNull(job.getSubmitTimestamp());
-	   assertNotNull(job.getOwnerEmail());
-	   assertNotNull(job.getOwnerId());
-	   assertNotNull(job.getExecutionInstanceId());
-	   assertNotNull(job.getResults());
-	   assertNotNull(job.getFlow());
-	   assertNotNull(job.getToken());
+		Job jobclone = (Job)job.clone();
+		
+		assertEquals(jobclone.getId(), job.getId());
+		System.out.println("clone id = " + jobclone.getId() + "job id = " +job.getId());
+		
+	   assertEquals(jobclone.getName(), job.getName());
+	   assertEquals(jobclone.getHost(), job.getHost());
+	   assertEquals(jobclone.getDescription(), job.getDescription());
+	   assertEquals(jobclone.getEndTimestamp(), job.getEndTimestamp());
+	   assertEquals(jobclone.getExecPort(), job.getExecPort());
+	   assertEquals(jobclone.getNumTries(), job.getNumTries());
+	   assertEquals(jobclone.getStatusCode(), job.getStatusCode());
+	   assertEquals(jobclone.getUpdateTimestamp(), job.getUpdateTimestamp());
+	   assertEquals(jobclone.getStartTimestamp(), job.getStartTimestamp());
+	   assertEquals(jobclone.getSubmitTimestamp(), job.getSubmitTimestamp());
+	   assertEquals(jobclone.getOwnerEmail(), job.getOwnerEmail());
+	   assertEquals(jobclone.getOwnerId(), job.getOwnerId());
+	   assertEquals(jobclone.getExecutionInstanceId(), job.getExecutionInstanceId());
+	   assertEquals(jobclone.getResults(), job.getResults());
+	   assertEquals(jobclone.getFlow(), job.getFlow());
+	   assertEquals(jobclone.getToken(), job.getToken());
 	   
-	   System.out.println(job.getId());
-	   System.out.println(job.getName());
-	   System.out.println(job.getHost());
-	   System.out.println(job.getDescription());
-	   System.out.println(job.getExecPort());
-	   System.out.println(job.getNumTries());
-	   System.out.println(job.getStatusCode());
-	   System.out.println("update time "+job.getUpdateTimestamp());
-	   System.out.println("start time " +job.getStartTimestamp());
-	   System.out.println("submit time  " + job.getSubmitTimestamp());
-	   System.out.println("end time " + job.getEndTimestamp());
-	   System.out.println(job.getOwnerEmail());
-	   System.out.println(job.getOwnerId());
-	   System.out.println(job.getExecutionInstanceId());
+	   System.out.println("clone = " + jobclone.getName() + "job = " +job.getName());
+	   System.out.println("clone = " + jobclone.getHost() + "job = " +job.getHost());
+	   System.out.println("clone = " + jobclone.getDescription() + "job = " +job.getDescription());
+	   System.out.println("clone = " + jobclone.getExecPort() + "job = " +job.getExecPort());
+	   System.out.println("clone = " + jobclone.getNumTries() + "job = " +job.getNumTries());
+	   System.out.println("clone = " + jobclone.getStatusCode() + "job = " +job.getStatusCode());
+	   System.out.println("clone = " + jobclone.getUpdateTimestamp() + "job update time "+job.getUpdateTimestamp());
+	   System.out.println("clone = " + jobclone.getStartTimestamp() + "job start time " +job.getStartTimestamp());
+	   System.out.println("clone = " + jobclone.getSubmitTimestamp() + "job submit time  " + job.getSubmitTimestamp());
+	   System.out.println("clone = " + jobclone.getEndTimestamp() + "job end time " + job.getEndTimestamp());
+	   System.out.println("clone = " + jobclone.getOwnerEmail() + "job = " +job.getOwnerEmail());
+	   System.out.println("clone = " + jobclone.getOwnerId() + "job = " +job.getOwnerId());
+	   System.out.println("clone = " + jobclone.getExecutionInstanceId() + "job = " +job.getExecutionInstanceId());
 	   
-	   for(JobResult jb:job.getResults())
+	   for(JobResult jb:jobclone.getResults())
 	   {
 		   System.out.println(jb.getResultType());
 		   System.out.println(jb.getId());
@@ -156,6 +167,6 @@ public class JobTest {
 	   System.out.println(job.getFlow().getDateCreated());
 	   System.out.println(job.getFlow().getCreatorId());
 		  
-	   System.out.println(job.getToken());
+	   System.out.println("clone token = " + jobclone.getToken() + "job token = "+job.getToken());
 	}
 }
