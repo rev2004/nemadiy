27da6731bbd8af071118851afc2c6500b04151be
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-03-28 12:47:07 +0000

    Added setup for lookup support.

--- resourceservice/trunk/lookup/run-reggie.sh
+++ resourceservice/trunk/lookup/run-reggie.sh
@@ -1,4 +1,5 @@
-POLICY="-Djava.security.policy=$CWD/reggie.policy"
-CONFIG_FILE=reggie.conf
+CWD=`pwd`
+POLICY="-Djava.security.policy=$CWD/config/reggie.policy"
+CONFIG_FILE=config/reggie.config
 MAIN_CLASS=org.imirsel.service.lookup.RunReggieLookup
 java -classpath .:${project.build.finalName}.jar $FLAG $POLICY  $MAIN_CLASS $CONFIG_FILE
