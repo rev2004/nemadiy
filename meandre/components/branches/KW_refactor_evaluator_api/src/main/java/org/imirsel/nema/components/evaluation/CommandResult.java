9b1e2f1a687237026aab7f9ef3fa9922621abde5
kris.west@gmail.com <kris.west@gmail.com@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-03-30 16:21:57 +0000

    Commenting out broken components (should go straight to java version rather than fixing this)

--- meandre/components/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/components/evaluation/CommandResult.java
+++ meandre/components/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/components/evaluation/CommandResult.java
@@ -1,19 +1,19 @@
-package org.imirsel.nema.components.evaluation;
-
-public class CommandResult {
-	public String getCsvFile() {
-		return csvFile;
-	}
-	public void setCsvFile(String csvFile) {
-		this.csvFile = csvFile;
-	}
-	public String getPngFile() {
-		return pngFile;
-	}
-	public void setPngFile(String pngFile) {
-		this.pngFile = pngFile;
-	}
-	private String csvFile;
-	private String pngFile;
-
-}
+//package org.imirsel.nema.components.evaluation;
+//
+//public class CommandResult {
+//	public String getCsvFile() {
+//		return csvFile;
+//	}
+//	public void setCsvFile(String csvFile) {
+//		this.csvFile = csvFile;
+//	}
+//	public String getPngFile() {
+//		return pngFile;
+//	}
+//	public void setPngFile(String pngFile) {
+//		this.pngFile = pngFile;
+//	}
+//	private String csvFile;
+//	private String pngFile;
+//
+//}
