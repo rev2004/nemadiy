73f4c229b50d497ffdfc2f23fd48aabe9dac2d62
kris.west@gmail.com <kris.west@gmail.com@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-03-31 22:49:45 +0000

    Merging changes from trunk in to branch

--- meandre/components/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/test/RemotePVANComponent.java
+++ meandre/components/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/test/RemotePVANComponent.java
@@ -1,8 +1,10 @@
-package org.imirsel.nema.test.components.remote;
+package org.imirsel.nema.test;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.imirsel.nema.components.InvalidProcessMonitorException;
 import org.imirsel.nema.components.InvalidProcessTemplateException;
@@ -11,29 +13,30 @@ import org.imirsel.nema.model.ProcessArtifact;
 import org.imirsel.nema.model.ProcessExecutionProperties;
 import org.imirsel.nema.monitor.process.NemaProcess;
 import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
 import org.meandre.annotations.ComponentOutput;
 import org.meandre.core.ComponentContext;
 import org.meandre.core.ComponentContextException;
 import org.meandre.core.ComponentContextProperties;
 import org.meandre.core.ComponentExecutionException;
 
-/**
- * 
- * @author kumaramit01
- * @since 0.2.0
- */
-@Component(creator = "Amit Kumar", description = "Test Remote Component Based on abstract class",
-		name = "ExtendedRemoteComponent", tags = "test remote abstract")
-public class ExtendedRemoteComponent extends RemoteProcessExecutorComponent {
+
+@Component(creator = "Mert Bay", description = "The remote execution component for phase vocoder analysis. C" 
+		, name = "RemotePVANComponent", tags = "remote execution, PVAN",
+		 firingPolicy = Component.FiringPolicy.all)
+public class RemotePVANComponent extends RemoteProcessExecutorComponent {
+
+	@ComponentInput(description="Input audio file", name="inputFile")
+	final static String DATA_INPUT_1= "inputFile";
 	
-	
-	@ComponentOutput(description = "This is the process artifact data",name = "processResult")
-	private static final String DATA_OUTPUT_1="processResult";
+	@ComponentOutput(description = "Output Phase Vocoder Analysis File. Input filename appended with .pv.an",name = "pvanOut")
+	private static final String DATA_OUTPUT_1="pvanOut";
 	
 	
 	public void initialize(ComponentContextProperties ccp)
-	throws ComponentExecutionException, ComponentContextException {
+	throws ComponentExecutionException, ComponentContextException {		
 		super.initialize(ccp);
+		
 	}
 	
 	public void dispose(ComponentContextProperties ccp)
@@ -47,20 +50,40 @@ public class ExtendedRemoteComponent extends RemoteProcessExecutorComponent {
 	@Override
 	public void execute(ComponentContext componentContext)
 			throws ComponentExecutionException, ComponentContextException {
-				ProcessArtifact pa = new ProcessArtifact("/tmp/0.date","file");
+			
+		/*
+				FileList object = (FileList) component.getDataComponentFromInput("TEST");
+				ResourceLocator locator=this.getResourceLocator();
+				String fileName=locator.materialize(object);
+				String fname=locator.findByTrackId(id);
+		*/	
+				
+			
+				String inputFile[] = (String[])componentContext.getDataComponentFromInput(DATA_INPUT_1);
+				
+				ProcessArtifact pa = new ProcessArtifact(inputFile[0]+".pv.an","file");
+				ProcessArtifact pa1 = new ProcessArtifact(inputFile[0],"file");
 				List<ProcessArtifact> outputs = new ArrayList<ProcessArtifact>();
 				outputs.add(pa);
+				List<ProcessArtifact> inputs = new ArrayList<ProcessArtifact>();
+				inputs.add(pa1);
 				ProcessExecutionProperties pep = new ProcessExecutionProperties();
 				pep.setId(componentContext.getExecutionInstanceID());
 				pep.setOutputs(outputs);
-				pep.setInputs(null);
-				//pep.setEnvironmentVariables(envorinmentVariables);
+				pep.setInputs(inputs);
+				Map<String,String> map = new HashMap<String,String>();
+				map.put("SNDANDIR","/share/apps/sndanweb/sndan");
+				pep.setEnvironmentVariables(map);
 				//pep.setCommandLineFlags(commandLineFlags);
 				
+				
+				
+				
+			
 		
 		
 		    try {
-		    	System.out.println("Running process now: " + this.getProfileName());
+		    	System.out.println("Running process now...");
 				@SuppressWarnings("unused")
 				final NemaProcess np=this.executeProcess(pep);
 				System.out.println("After running process. Now waiting for the process to finish.");
@@ -84,11 +107,12 @@ public class ExtendedRemoteComponent extends RemoteProcessExecutorComponent {
 				e.printStackTrace();
 				throw new ComponentExecutionException(e);
 			} catch (InvalidProcessTemplateException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
-				throw new ComponentExecutionException(e);
 			}
 		
 	
 	}
+
 
 }
