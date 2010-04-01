a43aa098398186aa28b630f2bf4b9886337b179c
kris.west <kris.west@gmail.com>
2010-04-01 02:09:47 +0000

    javadocs and removing need to specify an example path to capture filename extension

--- analytics/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/analytics/evaluation/OpaqueFileFormat.java
+++ analytics/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/analytics/evaluation/OpaqueFileFormat.java
@@ -5,25 +5,20 @@
 
 package org.imirsel.nema.analytics.evaluation;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Level;
-
 import org.imirsel.nema.model.*;
 import org.imirsel.nema.analytics.util.*;
-import org.imirsel.nema.analytics.util.io.DeliminatedTextFileUtilities;
 import org.imirsel.nema.analytics.util.io.IOUtil;
 
 /**
- * Reads and writes Raw audio files. When reading data is read into a byte[] and
- * wrapped in a NemaData Object. Writing the file out again recreates the audio 
- * file at the specified path.
+ * Reads and writes Raw data files. When reading data is read into a byte[] and
+ * wrapped in a NemaData Object. Writing the file out again recreates the 
+ * file exactly at the specified path.
+ * 
+ * Used to handle file formats that are opaque (i.e. we don't understand format 
+ * and can't read it, but need to move it around - e.g. model files).
  * 
  * @author kris.west@gmail.com
  * @since 0.1.0
@@ -32,16 +27,19 @@ public class OpaqueFileFormat extends SingleTrackEvalFileTypeImpl {
 
 	public static final String TYPE_NAME = "Opaque format file on disk";
 	
-	public OpaqueFileFormat(File path) {
+	public OpaqueFileFormat(/*File path*/) {
 		super(TYPE_NAME);
-		String name = path.getName();
-		int extIdx = name.lastIndexOf('.');
-		if (extIdx == -1) {
-			
-		}else{
-			String ext = name.substring(extIdx);
-			this.setFilenameExtension(ext);
-		}
+		//not really using the file extensions support at the moment - file 
+		  //paths are fully specified and extensions are not properly preserved 
+		  //according to the filenameExtension variable.
+//		String name = path.getName();
+//		int extIdx = name.lastIndexOf('.');
+//		if (extIdx == -1) {
+//			
+//		}else{
+//			String ext = name.substring(extIdx);
+//			this.setFilenameExtension(ext);
+//		}
 	}
 	
 	@Override
