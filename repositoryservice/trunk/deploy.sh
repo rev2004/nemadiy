d95d8b57a276763523c0030cb2efeb17c2e6f75a
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-04-10 02:21:08 +0000

    removed temp shell script

--- repositoryservice/trunk/deploy.sh
+++
@@ -1,2 +0,0 @@
-ant clean dist
-mvn deploy:deploy-file -Durl=scp://monk.lis.uiuc.edu:/content/web/root/snapshot -DrepositoryId=MonkSnapshotRepository -Dfile=dist/nema-repository-temp-0.1-SNAPSHOT.jar -DpomFile=pom.xml -Dversion=0.1-SNAPSHOT
--- repositoryservice/trunk/updatejavadoc.sh
+++
@@ -1,3 +0,0 @@
-javadoc -d apidocs -linksource -sourcepath src -classpath ./bin:. -package org.imirsel.nema.repository
-scp -r apidocs root@nema.lis.uiuc.edu:/var/www/html/developer/nema-temp-repository
-
