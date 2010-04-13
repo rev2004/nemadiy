591ec639631ce3ffa015be9c369efd97e9cec181
kris.west@gmail.com <kris.west@gmail.com@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-04-13 11:08:17 +0000

    Adding new path resolution and dataset insertion methods to repository interfaces

--- common/repositoryservice-api/trunk/src/main/java/org/imirsel/nema/repositoryservice/RepositoryUpdateClientInterface.java
+++ common/repositoryservice-api/trunk/src/main/java/org/imirsel/nema/repositoryservice/RepositoryUpdateClientInterface.java
@@ -112,15 +112,15 @@ public interface RepositoryUpdateClientInterface extends
     public void insertFileMetaLink(int file_id, int file_metadata_id) throws SQLException;
     
     /**
-     * Adds a dataset to the repository DB that contains only a single split and single set for that split
-     * (i.e. this dataset is just a list with divisions into sets or experiment iterations, e.g. an analysis 
-     * collection for onset detection or multi-F0 estimation). 
+     * Adds a dataset to the repository DB that contains only test sets e.g. an 
+     * analysis collection for onset detection or multi-F0 estimation. 
      * 
      * @param name The dataset name.
      * @param description The dataset description.
      * @param subject_track_metadata_type_id The subject track metadata or -1 if none.
      * @param filter_track_metadata_type_id The filter track metadata or -1 if none.
-     * @param datasetTrackIDList An (ordered) list of the trackID strings in the dataset.
+     * @param subsetList A list of all the track IDs appearing in the dataset.
+     * @param testLists An (ordered) list of the track IDs in each of the test sets.
      * @return The ID of the inserted dataset in the repository DB.
      * @throws SQLException
      */
@@ -128,7 +128,8 @@ public interface RepositoryUpdateClientInterface extends
             String description,
             int subject_track_metadata_type_id,
             int filter_track_metadata_type_id,
-            List<String> datasetTrackIDList) throws SQLException;
+            List<String> subsetList,
+            List<List<String>> testLists) throws SQLException;
     
     /**
      * Adds a dataset to the repository DB that contains only a single split and single set for that split
@@ -153,6 +154,30 @@ public interface RepositoryUpdateClientInterface extends
             int subject_track_metadata_type_id,
             int filter_track_metadata_type_id,
             File dataset_subset_file) throws SQLException, IOException;
+    
+    /**
+     * Adds a dataset to the repository DB that contains only a single split and single set for that split
+     * (i.e. this dataset is just a list with divisions into sets or experiment iterations, e.g. an analysis 
+     * collection for onset detection or multi-F0 estimation). 
+     * 
+     * The trackID list is read from a UTF-8 file on disk with one trackID per line. Other data on each line
+     * (delimited by whitespace) is ignored. TrackIDs maybe specified as full file paths, from which IMIRSEL 
+     * trackIDs (e.g. a002341) will be extracted.
+     * 
+     * @param name The dataset name.
+     * @param description The dataset description.
+     * @param subject_track_metadata_type_id The subject track metadata or -1 if none.
+     * @param filter_track_metadata_type_id The filter track metadata or -1 if none.
+     * @param dataset_subset_file A file listing the track IDs in the dataset with one track ID per line.
+     * @return The ID of the inserted dataset in the repository DB.
+     * @throws SQLException
+     * @throws IOException
+     */
+    public int insertTestOnlyDatasetFromDir(String name,
+            String description,
+            int subject_track_metadata_type_id,
+            int filter_track_metadata_type_id,
+            File dataset_dir) throws SQLException, IOException;
     
     /**
      * Adds a dataset to the repository DB that contains one or more splits of the dataset into
@@ -253,4 +278,16 @@ public interface RepositoryUpdateClientInterface extends
      */
     public void insertTrackListTracks(int trackListId, List<String> tracks) throws SQLException;
 	
+    /**
+     * Inserts a new task definition into the database.
+     * 
+     * @param name The name of the task.
+     * @param description A text description of the task.
+     * @param subjectTrackMetadataName The name of the metadata processed or 
+     * operated on by the task (which must already be defined in the DB).
+     * @param datasetId The id of the dataset which the task will operate on.
+     * @return the ID of the task inserted into the database.
+     * @throws SQLException
+     */
+    public int insertTask(String name, String description, String subjectTrackMetadataName, int datasetId)  throws SQLException;
 }
