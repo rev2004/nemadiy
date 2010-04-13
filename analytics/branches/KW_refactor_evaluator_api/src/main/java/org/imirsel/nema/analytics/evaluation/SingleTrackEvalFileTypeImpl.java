1f2f8788822589ab5cd5dc2843fff350b3e7a27f
kris.west@gmail.com <kris.west@gmail.com@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-04-13 11:10:24 +0000

    Supporting single file read in SingleTrackEvalFileType readDirectory method and adding efficient file copy method  to IOUtil

--- analytics/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/analytics/evaluation/SingleTrackEvalFileTypeImpl.java
+++ analytics/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/analytics/evaluation/SingleTrackEvalFileTypeImpl.java
@@ -29,38 +29,40 @@ public abstract class SingleTrackEvalFileTypeImpl extends NemaFileTypeImpl imple
 			throws IllegalArgumentException, FileNotFoundException, IOException{
 		
     	List<NemaData> out = new ArrayList<NemaData>();
-		
-		File[] files = theDir.listFiles();
-		ArrayList<File> filesToUse = new ArrayList<File>();
-		
-		getLogger().info("got " + files.length + " files for " + theDir.getAbsolutePath());
-		
-		if(extension == null){
-			for (int i = 0; i < files.length; i++){
-		        File file = files[i];
-		        if (!file.isDirectory()){
-		            filesToUse.add(file);
-		        }
-		    }
-		}else{
-			for (int i = 0; i < files.length; i++){
-		        File file = files[i];
-		        if (!file.isDirectory() && file.getName().endsWith(extension)){
-		            filesToUse.add(file);
-		        }
-		    }
+    	ArrayList<File> filesToUse = new ArrayList<File>();
+		if(theDir.isDirectory()) {
+			File[] files = theDir.listFiles();
+			
+			getLogger().info("got " + files.length + " files for " + theDir.getAbsolutePath());
+			
+			if(extension == null){
+				for (int i = 0; i < files.length; i++){
+			        File file = files[i];
+			        if (!file.isDirectory()){
+			            filesToUse.add(file);
+			        }
+			    }
+			}else{
+				for (int i = 0; i < files.length; i++){
+			        File file = files[i];
+			        if (!file.isDirectory() && file.getName().endsWith(extension)){
+			            filesToUse.add(file);
+			        }
+			    }
+			}
+			//this should sort results consistently across all submissions,
+			//   if they use the same names for their results files 
+			//   (otherwise there is no way to know if they are about the same test across different submissions)
+			Collections.sort(filesToUse);
+			
+			for(Iterator<File> it = filesToUse.iterator();it.hasNext();){
+				out.add(readFile(it.next()));
+			}
+			
+			getLogger().info("Retrieved " + out.size() + " of " + files.length + " files from " + theDir.getAbsolutePath());
+		}else {
+			out.add(readFile(theDir));
 		}
-		//this should sort results consistently across all submissions,
-		//   if they use the same names for their results files 
-		//   (otherwise there is no way to know if they are about the same test across different submissions)
-		Collections.sort(filesToUse);
-		
-		for(Iterator<File> it = filesToUse.iterator();it.hasNext();){
-			out.add(readFile(it.next()));
-		}
-		
-		getLogger().info("Retrieved " + out.size() + " of " + files.length + " files from " + theDir.getAbsolutePath());
-		
 		
 		return out;
 	}
--- analytics/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/analytics/util/io/IOUtil.java
+++ analytics/branches/KW_refactor_evaluator_api/src/main/java/org/imirsel/nema/analytics/util/io/IOUtil.java
@@ -40,6 +40,29 @@ public class IOUtil {
     private static final DecimalFormat MEMORY_FORMAT = new DecimalFormat("###,###,###,###.#");
     private static final double MEGABYTE_DIVISOR = 1024 * 1024;
 
+    
+    /**
+     * Copies one file into another.
+     * 
+     * @param in File to copy.
+     * @param out Location to copy it to.
+     * @throws IOException
+     */
+	public static void copyFile(File in, File out) throws IOException {
+		FileChannel inChannel = new FileInputStream(in).getChannel();
+		FileChannel outChannel = new FileOutputStream(out).getChannel();
+		try {
+			inChannel.transferTo(0, inChannel.size(), outChannel);
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			if (inChannel != null)
+				inChannel.close();
+			if (outChannel != null)
+				outChannel.close();
+		}
+	}
+    
     /** 
      * Read bytes from a File into a byte[].
      * 
