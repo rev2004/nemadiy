ff034df09b17feda6f5c1b8c54409a312d8e36e9
kris.west <kris.west@gmail.com>
2010-04-14 19:30:24 +0000

    Adding further unit test to command format parser

--- analytics/branches/KW_refactor_evaluator_api/src/test/java/org/imirsel/nema/analytics/util/process/CommandLineFormatParserTest.java
+++ analytics/branches/KW_refactor_evaluator_api/src/test/java/org/imirsel/nema/analytics/util/process/CommandLineFormatParserTest.java
@@ -68,4 +68,45 @@ public class CommandLineFormatParserTest {
 		
 		
 	}
+	
+	@Test
+	public void testClassificationFormatParsing() throws Exception {
+		String configString1 = "-v -x 1234 $i1{org.imirsel.nema.analytics.util.io.TrackListTextFile(bitrate=96k,sample-rate=22050)} " +
+				"-laln -o=$o1{org.imirsel.nema.analytics.evaluation.classification.ClassificationTextFile}";
+		String path1 = "/dummy/path1";
+		String path2 = "/dummy/path2";
+		String formatString1 = "-v -x 1234 /dummy/path1 -laln -o=/dummy/path2";
+		
+		CommandLineFormatParser parser = new CommandLineFormatParser(configString1);
+		
+		assert(parser.arguments != null);
+		System.out.println("Number of command argument parts: " + parser.arguments.size());
+		int count = 0;
+		for (Iterator<CommandArgument> iterator = parser.arguments.iterator(); iterator.hasNext();) {
+			CommandArgument arg = iterator.next();
+			System.out.println("\t" + count++ + ": " + arg.toConfigString()); 
+		}
+		
+		//check IO types
+		assert(parser.getInputType(1).equals(org.imirsel.nema.analytics.util.io.RawAudioFile.class));
+		assert(parser.getOutputType(1).equals(org.imirsel.nema.analytics.evaluation.melody.MelodyTextFile.class));
+		
+		//check properties
+		//TODO implement properties check
+		
+		//check config string
+		System.out.println("Original config string  : " + configString1);
+		String configString2 = parser.toConfigString();
+		System.out.println("Reproduced config string: " + configString2);
+		//can't do this as it doesn't matter what order properties come out in
+		//assert(configString1.equals(configString2));
+		
+		//check formatted string
+		parser.setPreparedPathForInput(1, path1);
+		parser.setPreparedPathForOutput(1, path2);
+		System.out.println("Correct formatted string:  " + formatString1);
+		String formatString2 = parser.toFormattedString();
+		System.out.println("Computed formatted string: " + formatString2);
+		assert(formatString1.equals(formatString2));	
+	}
 }
