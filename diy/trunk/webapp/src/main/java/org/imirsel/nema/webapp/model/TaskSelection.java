1736cfb69d5b4b56b5f66a2e3474976d87529bab
zggame <zggame@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-04-22 22:59:27 +0000

    add more webflow modification

---
+++ diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/model/TaskSelection.java
@@ -0,0 +1,20 @@
+package org.imirsel.nema.webapp.model;
+
+import java.io.Serializable;
+
+public class TaskSelection implements Serializable{
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+String id;
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+  
+}
