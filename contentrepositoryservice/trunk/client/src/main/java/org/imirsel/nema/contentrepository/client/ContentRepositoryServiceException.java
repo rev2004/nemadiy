167cd2dd3da4bec43bffd2c92fbe39f9d669efb9
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-04-26 19:19:13 +0000

    changed the method signatures to throw ContentRepositoryServiceException

---
+++ contentrepositoryservice/trunk/client/src/main/java/org/imirsel/nema/contentrepository/client/ContentRepositoryServiceException.java
@@ -0,0 +1,26 @@
+package org.imirsel.nema.contentrepository.client;
+
+/**Exception class encapsulates the Login and Repository Access exceptions
+ * 
+ * @author kumaramit01
+ * @since 0.0.1
+ */
+public class ContentRepositoryServiceException extends Exception {
+
+	/**Version of this class
+	 * 
+	 */
+	private static final long serialVersionUID = 6368349324314224732L;
+	
+	
+	public ContentRepositoryServiceException(Exception e){
+		super(e);
+	}
+	
+	
+	public ContentRepositoryServiceException(String message){
+		super(message);
+	}
+	
+	
+}
