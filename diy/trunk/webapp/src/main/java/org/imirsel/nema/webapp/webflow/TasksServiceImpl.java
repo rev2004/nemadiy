65e530878bc7c4f86cc6261c64d9e6c1a4d0216c
zggame <zggame@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-04-30 23:03:49 +0000

    add test result page

--- diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/webflow/TasksServiceImpl.java
+++ diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/webflow/TasksServiceImpl.java
@@ -25,11 +25,13 @@ import org.imirsel.nema.flowservice.MeandreServerException;
 import org.imirsel.nema.model.Component;
 import org.imirsel.nema.model.Flow;
 import org.imirsel.nema.model.Job;
+import org.imirsel.nema.model.JobResult;
 import org.imirsel.nema.model.Property;
 import org.imirsel.nema.model.Role;
 import org.imirsel.nema.model.User;
 import org.imirsel.nema.service.UserManager;
 import org.imirsel.nema.webapp.controller.JobController;
+import org.imirsel.nema.webapp.jobs.DisplayResultSet;
 import org.springframework.webflow.action.MultiAction;
 import org.springframework.webflow.core.collection.ParameterMap;
 import org.springframework.webflow.execution.Event;
@@ -60,19 +62,193 @@ public class TasksServiceImpl implements TasksService {
 		return true;
 	}
 
+	public String[] getRoles() {
+		Set<Role> roleList = this.userManager.getCurrentUser().getRoles();
+		int size = roleList.size();
 
+		String[] roles = new String[size];
+		int i = 0;
+		for (Role role : roleList) {
+			roles[i] = role.getName();
+			i++;
+		}
+		return roles;
+	}
 
-	public Long testRun(Flow flow, Map<String, String> parameters,
-			String name, String description) throws MeandreServerException {
+	public int test(String input) {
+		logger.debug(input);
+		return 1;
+	}
+
+	public Map<String, String> fillDefaultParameter(Flow flow) {
+
+		Map<String, String> parameters = new HashMap<String, String>();
+		List<Component> componentList = flowService
+				.getComponents(flow.getUri());
+		Collections.sort(componentList);
+		logger.info("componentList: " + componentList.size());
+		for (int i = 0; i < componentList.size(); i++) {
+			Component component = componentList.get(i);
+			Map<String, Property> m = flowService.getComponentPropertyDataType(
+					component, flow.getUri());
+			for (Entry<String, Property> entry : m.entrySet()) {
+				parameters.put(getName(component.getInstanceUri(), entry
+						.getKey()), entry.getValue().getDefaultValue()
+						.toString());
+			}
+		}
+		return parameters;
+	}
+
+	// TODO this method is the same as the one in ComponentPropertyTag, might
+	// need some
+	// refaction to get rid of one
+	private String getName(String component, String propertyName) {
+		if (component == null) {
+			return propertyName;
+		}
+		int index = component.lastIndexOf("/");
+		if (index == -1) {
+			return component + "_" + propertyName;
+		}
+		int second = component.substring(0, index).lastIndexOf("/");
+		String cname = component.substring(second + 1, index);
+		String count = component.substring(index + 1);
+		return cname + "_" + count + "_" + propertyName;
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<String, String> saveParameter(RequestContext context)
+			throws MeandreServerException {
+
+		logger.debug("start to save parameter");
+		Map<String, String> paramMap = (Map<String, String>) context
+				.getFlowScope().get("parameterMap");
+		Map<String, String> map = context.getRequestParameters().asMap();
+
+		try {
+
+			logger.debug("start to save parameters #" + map.size());
+			for (String name : map.keySet()) {
+				if (paramMap.containsKey(name)) {
+					logger.debug("replace parameter  (" + name + ":"
+							+ map.get(name) + ")");
+					paramMap.put(name, map.get(name));
+				}
+			}
+			return paramMap;
+		} catch (Exception e) {
+			logger.error(e, e);
+			throw new MeandreServerException(e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void saveParameterOld(RequestContext context)
+			throws MeandreServerException {
+		String token = System.currentTimeMillis() + "-token";
+
+		logger.debug("start to save parameter");
+		HttpServletRequest req = (HttpServletRequest) context
+				.getExternalContext().getNativeRequest();
+		ServletContext servletContext = (ServletContext) context
+				.getExternalContext().getNativeContext();
+
+		Map<String, String> paramMap = (Map<String, String>) context
+				.getFlowScope().get("parameterMap");
+
+		boolean isMultipart = ServletFileUpload.isMultipartContent(req);
+		if (!isMultipart) {
+			logger.error("Error -this should be multipart");
+			throw new MeandreServerException(
+					"the call to saveFlow should be multipart");
+		}
+
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		// upload.setSizeMax(yourMaxRequestSize);
+		String uploadDir = (servletContext).getRealPath(getUploadDirectory())
+				+ "/" + req.getRemoteUser() + "/" + token + "/";
+		// Create the directory if it doesn't exist
+		File dirPath = new File(uploadDir);
+
+		if (!dirPath.exists()) {
+			dirPath.mkdirs();
+		}
+
+		try {
+			List<FileItem> items = upload.parseRequest(req);
+			Iterator<FileItem> iter = items.iterator();
+			logger.debug("start to save parameters #" + items.size());
+			while (iter.hasNext()) {
+				FileItem item = iter.next();
+				if (item.isFormField()) {
+					String name = item.getFieldName();
+					String value = item.getString();
+					paramMap.put(name, value);
+
+				} else {
+					String fieldName = item.getFieldName();
+					String fileName = item.getName();
+					String contentType = item.getContentType();
+					boolean isInMemory = item.isInMemory();
+					long sizeInBytes = item.getSize();
+					if (fileName != null && sizeInBytes > 0
+							&& fileName.length() > 0) {
+						File uploadedFile = new File(uploadDir + File.separator
+								+ fileName);
+						item.write(uploadedFile);
+						logger.debug("file uploaded: " + fileName
+								+ uploadedFile.getAbsolutePath());
+						String webDir = uploadDir.substring(servletContext
+								.getRealPath("/").length());
+						paramMap.put(fieldName, "http://" + req.getServerName()
+								+ ":" + req.getServerPort()
+								+ req.getContextPath() + webDir + fileName);
+					}
+				}
+			}
+
+		} catch (FileUploadException e) {
+			logger.error(e, e);
+			throw new MeandreServerException(e);
+		} catch (Exception e) {
+			logger.error(e, e);
+			throw new MeandreServerException(e);
+		}
+
+	}
+
+	/**
+	 * Returns upload directory
+	 * 
+	 * @return upload directory
+	 */
+	public String getUploadDirectory() {
+		return uploadDirectory;
+	}
+
+	/**
+	 * Set the upload directory
+	 * 
+	 * @param uploadDirectory
+	 */
+	public void setUploadDirectory(String uploadDirectory) {
+		this.uploadDirectory = uploadDirectory;
+	}
+
+	public Job testRun(Flow flow, Map<String, String> parameters, String name,
+			String description) throws MeandreServerException {
 		String token = System.currentTimeMillis() + "-token";
 		HashMap<String, String> paramMap = new HashMap<String, String>(
 				parameters);
 		String flowId = flow.getId().toString();
-		String flowUri =flow.getUri();
+		String flowUri = flow.getUri();
 
 		logger.debug("start to test run");
 		if (flowId == null || flowUri == null) {
-			logger.error("flowId or flowUri is null -some severe error happened...");
+			logger
+					.error("flowId or flowUri is null -some severe error happened...");
 			throw new MeandreServerException("flowId or flowUri is null");
 		}
 		User user = userManager.getCurrentUser();
@@ -118,187 +294,37 @@ public class TasksServiceImpl implements TasksService {
 		Job job = this.flowService.executeJob(token, name, description,
 				instanceId, user.getId(), user.getEmail());
 
-		if (job!=null) return job.getId();
-		else return null;
-		
-	}
-
-	public String[] getRoles() {
-		Set<Role> roleList = this.userManager.getCurrentUser().getRoles();
-		int size = roleList.size();
-
-		String[] roles = new String[size];
-		int i = 0;
-		for (Role role : roleList) {
-			roles[i] = role.getName();
-			i++;
-		}
-		return roles;
-	}
-
-	public int test(String input) {
-		logger.debug(input);
-		return 1;
-	}
-
-	public Map<String, String> fillDefaultParameter(Flow flow) {
-
-		Map<String, String> parameters = new HashMap<String, String>();
-		List<Component> componentList = flowService
-				.getComponents(flow.getUri());
-		Collections.sort(componentList);
-		logger.info("componentList: " + componentList.size());
-		for (int i = 0; i < componentList.size(); i++) {
-			Component component=componentList.get(i);
-			Map<String, Property> m = flowService.getComponentPropertyDataType(
-					component, flow.getUri());
-			for (Entry<String, Property> entry : m.entrySet()) {
-				parameters.put(getName(component.getInstanceUri(), entry.getKey()), entry.getValue()
-						.getDefaultValue().toString());
-			}
-		}
-		return parameters;
-	}
-	
-	
-	//TODO this method is the same as the one in ComponentPropertyTag, might need some 
-	//   	refaction to get rid of one
-	private String getName(String component,String propertyName){
-		if(component==null){
-			return propertyName;
-		}
-		int index = component.lastIndexOf("/");
-		if(index==-1){
-			return component+"_"+propertyName;
-		}
-		int second = component.substring(0, index).lastIndexOf("/");
-		String cname=component.substring(second+1,index);
-		String count = component.substring(index+1);
-		return cname+"_"+count+"_"+propertyName;
-	}
-
-	@SuppressWarnings("unchecked")
-	public Map<String,String> saveParameter(RequestContext context)
-			throws MeandreServerException {
-	
-		logger.debug("start to save parameter");
-		Map<String, String> paramMap = (Map<String, String>) context
-				.getFlowScope().get("parameterMap");
-		Map<String,String> map=context.getRequestParameters().asMap();
-
-		try {
-
-			logger.debug("start to save parameters #"+map.size());
-			for (String name:map.keySet()) {
-				if (paramMap.containsKey(name)){
-					logger.debug("replace parameter  ("+name+":"+map.get(name)+")");
-					paramMap.put(name, map.get(name));
-				}
-			}
-			return paramMap;
-		}  catch (Exception e) {
-			logger.error(e, e);
-			throw new MeandreServerException(e);
-		}
-	}
-	
-	
-	@SuppressWarnings("unchecked")
-	public void saveParameterOld(RequestContext context)
-			throws MeandreServerException {
-		String token = System.currentTimeMillis() + "-token";
-
-		logger.debug("start to save parameter");
-		HttpServletRequest req = (HttpServletRequest) context
-				.getExternalContext().getNativeRequest();
-		ServletContext servletContext = (ServletContext) context
-				.getExternalContext().getNativeContext();
-
-		Map<String, String> paramMap = (Map<String, String>) context
-				.getFlowScope().get("parameterMap");
-
-		boolean isMultipart = ServletFileUpload.isMultipartContent(req);
-		if (!isMultipart) {
-			logger.error("Error -this should be multipart");
-			throw new MeandreServerException(
-					"the call to saveFlow should be multipart");
-		}
-
-		DiskFileItemFactory factory = new DiskFileItemFactory();
-		ServletFileUpload upload = new ServletFileUpload(factory);
-		// upload.setSizeMax(yourMaxRequestSize);
-		String uploadDir = (servletContext).getRealPath(getUploadDirectory())
-				+ "/" + req.getRemoteUser() + "/" + token + "/";
-		// Create the directory if it doesn't exist
-		File dirPath = new File(uploadDir);
-
-		if (!dirPath.exists()) {
-			dirPath.mkdirs();
-		}
-
-		try {
-			List<FileItem> items = upload.parseRequest(req);
-			Iterator<FileItem> iter = items.iterator();
-			logger.debug("start to save parameters #"+items.size());
-			while (iter.hasNext()) {
-				FileItem item = iter.next();
-				if (item.isFormField()) {
-					String name = item.getFieldName();
-					String value = item.getString();
-					paramMap.put(name, value);
-
-				} else {
-					String fieldName = item.getFieldName();
-					String fileName = item.getName();
-					String contentType = item.getContentType();
-					boolean isInMemory = item.isInMemory();
-					long sizeInBytes = item.getSize();
-					if (fileName != null && sizeInBytes > 0
-							&& fileName.length() > 0) {
-						File uploadedFile = new File(uploadDir + File.separator
-								+ fileName);
-						item.write(uploadedFile);
-						logger.debug("file uploaded: " + fileName
-								+ uploadedFile.getAbsolutePath());
-						String webDir = uploadDir.substring(servletContext
-								.getRealPath("/").length());
-						paramMap.put(fieldName, "http://" + req.getServerName()
-								+ ":" + req.getServerPort()
-								+ req.getContextPath() + webDir + fileName);
-					}
-				}
-			}
-
-		} catch (FileUploadException e) {
-			logger.error(e, e);
-			throw new MeandreServerException(e);
-		} catch (Exception e) {
-			logger.error(e, e);
-			throw new MeandreServerException(e);
-		}
+		return job;
 
 	}
-	/**
-	 * Returns upload directory
-	 * 
-	 * @return upload directory
-	 */
-	public String getUploadDirectory() {
-		return uploadDirectory;
-	}
 
-	/**
-	 * Set the upload directory
-	 * 
-	 * @param uploadDirectory
-	 */
-	public void setUploadDirectory(String uploadDirectory) {
-		this.uploadDirectory = uploadDirectory;
-	}
-
-	public Long run(Flow flow, Map<String, String> parameters, String name,
+	public Job run(Flow flow, Map<String, String> parameters, String name,
 			String description) throws MeandreServerException {
-		return this.testRun(flow,parameters,name,description);
+		return this.testRun(flow, parameters, name, description);
+	}
+
+	public DisplayResultSet getJobResult(Job job) {
+		Set<JobResult> results = job.getResults();
+		if (results == null)
+			return null;
+		else {
+			for (JobResult result : results) {
+				logger.debug("RESULT: " + result.getUrl() + "  "
+						+ result.getId());
+				result.setUrl(processUrl(result.getUrl()));
+
+			}
+			DisplayResultSet resultSet = new DisplayResultSet(results);
+			return resultSet;
+		}
+	}
+
+	private String processUrl(String url) {
+		String identifier = "published_resources/nema";
+		int index = url.indexOf(identifier);
+		String resultFolder = url.substring(index + identifier.length());
+		return "http://nema.lis.uiuc.edu/nema_out" + resultFolder;
+
 	}
 
 }
