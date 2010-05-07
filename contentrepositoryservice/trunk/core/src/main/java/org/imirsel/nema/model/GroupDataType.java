7a3d994660e19b5e8a80b740160b64f971c86619
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-05-07 14:44:38 +0000

    added resource type service

---
+++ contentrepositoryservice/trunk/core/src/main/java/org/imirsel/nema/model/GroupDataType.java
@@ -0,0 +1,27 @@
+package org.imirsel.nema.model;
+
+public class GroupDataType {
+	private String name;
+	private String value;
+
+	public GroupDataType(String string, String string2) {
+		this.name=name;
+		this.value=value;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
