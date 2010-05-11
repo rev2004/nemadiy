d600b323eb47a019f328cc51c0ef3f1ca942b29b
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-05-11 22:34:39 +0000

    Updated ProcessExecutorService -added new method to support dynamic profiles. Updated NemaExecutorApp to support jcr. Added JCR support -ContentRepositoryUtil -preliminary. Needs a lot of rework

---
+++ common/flowservice-api/branches/0.6.0/tag.sh
@@ -0,0 +1,3 @@
+release=0.5.0
+module=common/flowservice-api
+svn copy https://nemadiy.googlecode.com/svn/$module/trunk https://nemadiy.googlecode.com/svn/$module/branches/$release -m "Creating 0.5.0 branch"
