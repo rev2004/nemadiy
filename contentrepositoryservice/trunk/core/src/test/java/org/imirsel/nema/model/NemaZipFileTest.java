f1b6c08b6cb3d178b0075f2895195aa8a4d4ac6b
shirk993 <shirk993@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-05-12 21:55:43 +0000

    Added some tests.

--- contentrepositoryservice/trunk/core/src/test/java/org/imirsel/nema/model/NemaZipFileTest.java
+++ contentrepositoryservice/trunk/core/src/test/java/org/imirsel/nema/model/NemaZipFileTest.java
@@ -3,6 +3,8 @@ package org.imirsel.nema.model;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.util.jar.JarFile;
 import java.util.zip.ZipFile;
 
 import org.imirsel.nema.test.BaseManagerTestCase;
@@ -14,10 +16,15 @@ import org.junit.Test;
 
 public class NemaZipFileTest extends BaseManagerTestCase {
 
+   ZipFile zipFile;
    NemaZipFile nemaZipFile;
+   
+   JarFile jarFile;
+   NemaZipFile nemaJarFile;
    
    String zipFileLocation = "core/src/test/resources";
    String zipFileName = "testZipFile.zip";
+   String jarFileName = "testJarFile.jar";
    String fileSep = System.getProperty("file.separator");
    
    @BeforeClass
@@ -31,14 +38,26 @@ public class NemaZipFileTest extends BaseManagerTestCase {
    @Before
    public void setUp() throws Exception {
       String workingDir = System.getProperty("user.dir"); 
-      ZipFile zipFile = new ZipFile(workingDir + fileSep + zipFileLocation + fileSep + zipFileName);
+      zipFile = new ZipFile(workingDir + fileSep + zipFileLocation + fileSep + zipFileName);
       nemaZipFile = new NemaZipFile(zipFile);
       nemaZipFile.open();
+      
+      jarFile = new JarFile(workingDir + fileSep + zipFileLocation + fileSep + jarFileName);
+      nemaJarFile = new NemaZipFile(jarFile);
+      nemaJarFile.open();
+      
+      System.out.println(nemaJarFile.getSourceZipContentDir());
    }
 
    @After
    public void tearDown() throws Exception {
       nemaZipFile.close();
+      nemaJarFile.close();
+   }
+   
+   @Test(expected=java.lang.IllegalArgumentException.class)
+   public void testIllegalUnzipDir() throws IOException {
+      NemaZipFile testZipFile = new NemaZipFile(zipFile,"/a/path/that/does/not/exist");
    }
    
    @Test
@@ -46,6 +65,10 @@ public class NemaZipFileTest extends BaseManagerTestCase {
       assertFalse(nemaZipFile.containsFile("struts-console-3.4/aFakeFileName.txt"));
       assertTrue(nemaZipFile.containsFile("struts-console-3.4/bin/console.sh"));
       assertTrue(nemaZipFile.containsFile("struts-console-3.4/org.apache.struts.console/lib/console.jar"));
+      
+      assertFalse(nemaJarFile.containsFile("some/bullshit/class/name/Class.class"));
+      assertTrue(nemaJarFile.containsFile("LICENSE.txt"));
+      assertTrue(nemaJarFile.containsFile("org/apache/struts/config/ActionConfig.class"));
    }
 
    @Test
@@ -53,11 +76,16 @@ public class NemaZipFileTest extends BaseManagerTestCase {
       assertFalse(nemaZipFile.containsClass("org.nobody.fake.FooBar"));
       assertFalse(nemaZipFile.containsClass("org.apache.struts.console.ChildNode.class"));
       assertTrue(nemaZipFile.containsClass("org.apache.struts.console.ChildNode"));
+
+      assertFalse(nemaJarFile.containsClass("org.apache.struts.config.Bullshit"));
+      assertFalse(nemaJarFile.containsClass("org.apache.struts.config.ActionConfig.class"));
+      assertTrue(nemaJarFile.containsClass("org.apache.struts.config.ActionConfig"));
    }
    
    @Test
    public void testContainsJars() {
       assertTrue(nemaZipFile.containsJars());
+      assertFalse(nemaJarFile.containsJars());
    }
    
    @Test
