75ebbed8d94c98a2ce800a64722d80d09dd48021
shirk993 <shirk993@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-05-20 20:20:41 +0000

    Fixed an error

--- diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/webflow/validator/UploadedExecutableBundleValidator.java
+++ diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/webflow/validator/UploadedExecutableBundleValidator.java
@@ -1,6 +1,7 @@
 package org.imirsel.nema.webapp.webflow.validator;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import org.springframework.binding.message.MessageBuilder;
 import org.springframework.binding.message.MessageContext;
@@ -14,65 +15,74 @@ import org.imirsel.nema.webapp.model.UploadedExecutableBundle;
  * @author shirk
  */
 public class UploadedExecutableBundleValidator {
-
+   private static final Logger logger = 
+      Logger.getLogger(UploadedExecutableBundleValidator.class.getName());
+   
    public void validateUpload(UploadedExecutableBundle uploadedExecutable,
          ValidationContext context) {
       MessageContext messages = context.getMessageContext();
 
       String fileName = uploadedExecutable.getFileName();
       String fileExtension = fileName.substring(fileName.length() - 3);
-      boolean failedValidation = false;
 
+      // First make sure the uploaded file type matches what is allowed for
+      // the selected executable type.
       if (uploadedExecutable.getType() == ExecutableType.JAVA) {
          if (!fileExtension.equalsIgnoreCase("jar")
                && !fileExtension.equalsIgnoreCase("zip")) {
-            failedValidation = true;
             messages.addMessage(new MessageBuilder().error().source("file")
                   .defaultText(
                         "Invalid file type. For Java "
                               + "executables, only JAVA or ZIP files are "
                               + "allowed.").build());
+            return;
          }
       } else {
          if (!fileExtension.equalsIgnoreCase("zip")) {
-            failedValidation = true;
+            messages
+                  .addMessage(new MessageBuilder().error().source("file")
+                        .defaultText(
+                              "Invalid file type. Only ZIP files are allowed.")
+                        .build());
+            return;
+         }
+      }
+
+      // Next make sure the file is readable in ZIP format. Ensure not corrupt.
+      boolean isReadable = false;
+      try {
+         isReadable = uploadedExecutable.readableAsZip();
+      } catch (IOException e) {
+         logger.warning(e.getMessage());
+      }
+
+      if (!isReadable) {
+         messages.addMessage(new MessageBuilder().error().source("file")
+               .defaultText("Uploaded file is corrupt or cannot be read.")
+               .build());
+         return;
+      }
+
+      // Finally, validate the the specified executable file exists within
+      // the uploaded archive.
+      try {
+         if (!uploadedExecutable.containsExecutable()) {
+            String exeFileType;
+            if (uploadedExecutable.getType() == ExecutableType.JAVA) {
+               exeFileType = "main class";
+            } else {
+               exeFileType = "executable file";
+            }
             messages.addMessage(new MessageBuilder().error().source("file")
                   .defaultText(
-                        "Invalid file type. Only ZIP files are allowed.")
+                        "The specified " + exeFileType
+                              + " was not found in the uploaded archive.")
                   .build());
+            return;
          }
+      } catch (IOException e) {
+         logger.warning(e.getMessage());
       }
-
-      if (!failedValidation) {
-         if (!uploadedExecutable.passesIntegrityCheck()) {
-            failedValidation = true;
-            messages.addMessage(new MessageBuilder().error().source("file")
-                  .defaultText("Uploaded file is corrupt or cannot be read.")
-                  .build());
-         }
-
-         if (!failedValidation) {
-            try {
-               if (!uploadedExecutable.containsExecutable()) {
-                  failedValidation = true;
-                  String exeFileType;
-                  if(uploadedExecutable.getType()==ExecutableType.JAVA) {
-                     exeFileType = "main class";
-                  } else {
-                     exeFileType = "executable file";
-                  }
-                  messages.addMessage(new MessageBuilder().error().source(
-                        "file").defaultText(
-                        "The specified " + exeFileType + " was not found in the uploaded archive.")
-                        .build());
-               }
-            } catch (IOException e) {
-               e.printStackTrace();
-            }
-         }
-      }
-      
-      System.out.println("-------------> Passed validation?   " + !failedValidation);
    }
 
    public boolean isNullOrEmpty(String str) {
