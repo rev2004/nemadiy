1a14b8705d110186ab0178455c1276e1a30fb117
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-06-04 02:53:21 +0000

    update

---
+++ meandre/nema-client/trunk/tag.sh
@@ -0,0 +1,3 @@
+release=0.5.0
+module=meandre/nema-client
+svn copy https://nemadiy.googlecode.com/svn/$module/trunk https://nemadiy.googlecode.com/svn/$module/branches/$release -m "Creating 0.5.0 branch"
