120e69b85d32003c126ce5ba7b21d9b8cc391414
zggame <zggame@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-06-11 22:10:54 +0000

    a few tweaks on UI, xml scheme

--- diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/webflow/TasksServiceImpl.java
+++ diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/webflow/TasksServiceImpl.java
@@ -516,26 +516,6 @@ public class TasksServiceImpl {
          tmpProps.remove(GROUP);
          tmpProps.remove(OS);
       }
-      
-      //Map<String, Property> formattedProps = new TreeMap<String, Property>();
-      
-      // Title case the property names.
-//      for (Map.Entry<String, Property> entry : tmpProps.entrySet()) {
-//         String key = entry.getKey();
-//         
-//         assert !key.isEmpty():"key is empty";
-//         StringBuilder newkey=new StringBuilder(key.substring(0, 1).toUpperCase());
-//         for (int i=1;i<key.length();i++){
-//          boolean currentCharIsUpper = Character.isUpperCase(key.charAt(i));
-//          boolean previousCharIsNotUpper = !Character.isUpperCase(key.charAt(i-1));
-//          boolean previousCharIsNotSpace = key.charAt(i-1)!=' ';
-//        	 if (currentCharIsUpper && previousCharIsNotUpper && previousCharIsNotSpace){
-//        		 newkey.append(" ");
-//        	 }
-//        	 newkey.append(key.charAt(i));
-//         }
-//         formattedProps.put(newkey.toString(), entry.getValue());
-//      }
       return tmpProps;
    }
 
