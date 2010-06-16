f2486d5bd9f3e0574825b3d5a827333bd75ed583
afe405 <afe405@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-06-16 18:48:40 +0000

    placeholder for struct results renderer

---
+++ analytics/trunk/src/main/java/org/imirsel/nema/analytics/evaluation/structure/StructureResultsRenderer.java
@@ -0,0 +1,17 @@
+package org.imirsel.nema.analytics.evaluation.structure;
+
+import java.io.IOException;
+
+import org.imirsel.nema.analytics.evaluation.ResultRendererImpl;
+import org.imirsel.nema.model.NemaEvaluationResultSet;
+
+public class StructureResultsRenderer extends ResultRendererImpl {
+
+	@Override
+	public void renderResults(NemaEvaluationResultSet results)
+			throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
