b7410a008be5336361d138aef7da7da4fbea5ab9
zggame <zggame@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-06-26 00:03:27 +0000

    working on the json response

---
+++ diy/trunk/webapp/src/main/java/org/imirsel/nema/webapp/json/ConverterToMapServer.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.imirsel.nema.webapp.json;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.imirsel.nema.flowservice.config.MeandreServerProxyConfig;
+import org.imirsel.nema.flowservice.config.MeandreServerProxyStatus;
+import org.imirsel.nema.model.Job;
+
+/**
+ * @author gzhu1
+ * 
+ */
+public class ConverterToMapServer
+		implements
+		ConverterToMap<Entry<MeandreServerProxyConfig, MeandreServerProxyStatus>> {
+
+	@Override
+	public Map<String, String> convertToMap(
+			Entry<MeandreServerProxyConfig, MeandreServerProxyStatus> entry) {
+		
+		ConverterToMapServerConfig converter=new ConverterToMapServerConfig();
+		Map<String, String> map = converter.convertToMap(entry.getKey());
+
+		JsonHelper helper = new JsonHelper(map);
+
+		
+		helper.addCheckNull(entry.getValue().getNumAborting(), "numAborting");
+		helper.addCheckNull(entry.getValue().getNumRunning(), "numRunning");
+
+		return map;
+	}
+
+}
