8a56580c5d90156d80f7cf2904ecefa41125a3bc
afe405 <afe405@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-06-28 20:43:08 +0000

    added update to support the reporting of just the two tempi and no salience. salience is irrelevant for the algorithms.

--- analytics/trunk/src/main/java/org/imirsel/nema/analytics/evaluation/tempo/TempoTextFile.java
+++ analytics/trunk/src/main/java/org/imirsel/nema/analytics/evaluation/tempo/TempoTextFile.java
@@ -46,9 +46,9 @@ public class TempoTextFile extends SingleTrackEvalFileTypeImpl {
 			throw new IllegalArgumentException(msg);
 		}
 		
-		if (tempoDataStrArray[0].length != 3) {
+		if (tempoDataStrArray[0].length != 3 || tempoDataStrArray[0].length != 2) {
 			String msg = "This file could not be parsed into separate tempo1, tempo2, and tempo salience! " +
-					"Format should be <tempo1>\t<tempo2>\t<salience>\n<EOF>. Content: \n";
+					"Format should be <tempo1>\t<tempo2>\t<salience>\n<EOF> OR <tempo1>\t<tempo2>\n<EOF>. Content: \n";
 			for (int i = 0; i < tempoDataStrArray[0].length; i++) {
 				msg += "'" + tempoDataStrArray[0][i] + "'";
 				if (i<tempoDataStrArray[0].length-1){
