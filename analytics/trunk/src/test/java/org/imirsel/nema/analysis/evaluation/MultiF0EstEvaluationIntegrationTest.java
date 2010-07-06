d51dc94bab8e076c2a9f63f8352999f7856596da
mertbay <mertbay@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-07-06 16:37:56 +0000

    mf0Est test

--- analytics/trunk/src/test/java/org/imirsel/nema/analysis/evaluation/MultiF0EstEvaluationIntegrationTest.java
+++ analytics/trunk/src/test/java/org/imirsel/nema/analysis/evaluation/MultiF0EstEvaluationIntegrationTest.java
@@ -17,6 +17,8 @@ import org.imirsel.nema.analytics.evaluation.ResultRenderer;
 import org.imirsel.nema.analytics.evaluation.ResultRendererFactory;
 import org.imirsel.nema.analytics.evaluation.SingleTrackEvalFileType;
 import org.imirsel.nema.analytics.evaluation.melody.MelodyTextFile;
+import org.imirsel.nema.analytics.evaluation.multif0.MultiF0EstTextFile;
+
 import org.imirsel.nema.model.NemaData;
 import org.imirsel.nema.model.NemaDataConstants;
 import org.imirsel.nema.model.NemaDataset;
@@ -109,37 +111,50 @@ public class MultiF0EstEvaluationIntegrationTest extends BaseManagerTestCase{
     }
 	
 	
+
+
 	@Test
-	public void testEvaluateKDTwoFolds()  throws IllegalArgumentException, IOException, InstantiationException, IllegalAccessException{ 
-		File groundTruthDirectory = new File("src/test/resources/melody/groundtruth");
-		File resultsDirectory1 = new File("src/test/resources/melody/KD-2-fold/fold1");
-		File resultsDirectory2 = new File("src/test/resources/melody/KD-2-fold/fold2");
-		String	systemName = "SystemName";
+	public void testEvaluateManySystems()  throws IllegalArgumentException, IOException, InstantiationException, IllegalAccessException{ 
+
+		
+		File groundTruthDirectory = new File("src/test/resources/multiF0Est/groundtruth");
+		File resultsDirectory = new File("src/test/resources/multiF0Est/NEOS1");
+		
+		List<File> systemDirs = new ArrayList<File>();
+		List<String> systemNames = new ArrayList<String>();
+		File [] files = resultsDirectory.listFiles();
+        for (int i = 0; i < files.length; i++) {
+			if(files[i].isDirectory() && !(files[i].getName().equals(".svn"))){
+				String systemName = files[i].getName();
+				
+				System.out.println("got system: " + systemName);
+				systemDirs.add(files[i]);
+				systemNames.add(systemName);
+			}
+		}
+		
 		Evaluator evaluator = null;
 		ResultRenderer renderer = null;
 		
 		//evaluator = new MelodyEvaluator(task, dataset, outputDirectory, workingDirectory, testSets);
-		evaluator = EvaluatorFactory.getEvaluator(twoSetTask.getSubjectTrackMetadataName(), twoSetTask, twoSetDataset, null, twoTestSets);
-		renderer = ResultRendererFactory.getRenderer(twoSetTask.getSubjectTrackMetadataName(), outputDirectory, workingDirectory, false, null);
-		SingleTrackEvalFileType reader = new MelodyTextFile();
+		evaluator = EvaluatorFactory.getEvaluator(singleSetTask.getSubjectTrackMetadataName(), singleSetTask, singleSetDataset, null, singleTestSet);
+		renderer = ResultRendererFactory.getRenderer(singleSetTask.getSubjectTrackMetadataName(), outputDirectory, workingDirectory, false, null);
+		SingleTrackEvalFileType reader = new MultiF0EstTextFile();
 		
 		List<NemaData> groundTruth = reader.readDirectory(groundTruthDirectory, ".txt");
 		evaluator.setGroundTruth(groundTruth);
 	
-		List<NemaData> results1 = reader.readDirectory(resultsDirectory1, null);
-		List<NemaData> results2 = reader.readDirectory(resultsDirectory2, null);
+		for (int i = 0; i < systemNames.size(); i++) {
+			List<NemaData> resultsForAllTracks = reader.readDirectory(systemDirs.get(i), null);
+			evaluator.addResults(systemNames.get(i), systemNames.get(i), singleTestSet.get(0), resultsForAllTracks);
+		}
 		
-		//divide into the two folds
+	
+		NemaEvaluationResultSet results = evaluator.evaluate();
+		assertTrue(results != null);
 		
-		evaluator.addResults(systemName, "**FlowID**", twoTestSets.get(0), results1);
-		evaluator.addResults(systemName, "**FlowID**", twoTestSets.get(1), results2);
-		
-		NemaEvaluationResultSet evalResults = evaluator.evaluate();
-		assertTrue(evalResults != null);
-		
-
 		//test rendering
-		renderer.renderResults(evalResults);
+		renderer.renderResults(results);
 		
 	  //File resultFile = new File("src/test/resources/classification/evaluation/GT1/report.txt");
 	  //File outputFile = new File(outputDirectory,systemName+System.getProperty("file.separator")+"report.txt");
@@ -147,8 +162,6 @@ public class MultiF0EstEvaluationIntegrationTest extends BaseManagerTestCase{
 	  //assertThat(resultFile, fileContentEquals(outputFile));
 	
 	}
-	
-
 	
 	
 	
