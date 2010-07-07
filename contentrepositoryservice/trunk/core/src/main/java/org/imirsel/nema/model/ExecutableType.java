215ec4544995f53e46fed4a48308f7d34caeb859
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-07-07 15:41:10 +0000

    modified the executable type enum constructor

--- contentrepositoryservice/trunk/core/src/main/java/org/imirsel/nema/model/ExecutableType.java
+++ contentrepositoryservice/trunk/core/src/main/java/org/imirsel/nema/model/ExecutableType.java
@@ -1,18 +1,26 @@
 package org.imirsel.nema.model;
 
+import java.io.Serializable;
+
 /**
  * @author kumaramit01
- * @since 0.5.0 -extracted from the class -shared with the executor service
+ * @since 0.4.0 -extracted from the class -shared with the executor service
+ * @version 0.4.0 -Added versionString for the the supported executables
  */
-public enum ExecutableType {
-	JAVA("Java",0), MATLAB("Matlab",1), BIN("Bin",2), SHELL("Shell",3), PYTHON("Python",4), PERL("Perl",5), WINE("Wine",6), RUBY("Ruby",7);
+public enum ExecutableType implements Serializable {
+	JAVA("Java","-version",0), MATLAB("Matlab","-nosplash -nodesktop  -nojvm -nodisplay -r \"version,exit\"",1),
+	BIN("Bin","-version",2), SHELL("Shell","-version",3), PYTHON("Python","-V",4), 
+	PERL("Perl","-v",5), WINE("Wine","--version",6), RUBY("Ruby","--version",7);
 
   private String name;
+  private String versionString;
   private int code;
+  private String versionCmdLine;
   
-	private ExecutableType(String name,int code) {
+	private ExecutableType(String name, String versionCmdLine,int code) {
 		this.name = name;
 		this.code = code;
+		this.versionCmdLine = versionCmdLine;
 	}
 
 	public String getName() {
@@ -26,6 +34,20 @@ public enum ExecutableType {
 	@Override
 	public String toString() {
 		return name;
+	}
+	
+	
+	public void setVersionString(String version){
+		this.versionString = version;
+	}
+	
+	public String getVersionString(){
+		return versionString;
+	}
+	
+	
+	public String getVersionCmdLine(String typeName){
+		return versionCmdLine;
 	}
 
 	public static ExecutableType toExecutableType(String typeName) {
