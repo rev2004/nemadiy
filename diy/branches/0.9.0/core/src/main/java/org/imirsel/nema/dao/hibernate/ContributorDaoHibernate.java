c3e5d51c2be391bba5cf2cfa5e1f97926e72527a
zggame <zggame@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-07-12 05:44:09 +0000

    Adjust annotation and controllers

--- diy/branches/0.9.0/core/src/main/java/org/imirsel/nema/dao/hibernate/ContributorDaoHibernate.java
+++ diy/branches/0.9.0/core/src/main/java/org/imirsel/nema/dao/hibernate/ContributorDaoHibernate.java
@@ -31,7 +31,7 @@ public class ContributorDaoHibernate extends GenericDaoHibernate<Contributor, Lo
 
 	public List<Contributor> findSimilar(String str) {
 		List list = 
-			getHibernateTemplate().find("from Contributor where (firstname like ?) or (lastname like ?) or (orgnization like ?)",
+			getHibernateTemplate().find("from Contributor where (firstname like '%?%') or (lastname like '%?%') or (orgnization like '%?%')",
 					str,str,str);
 		return list;
 	}
--- diy/branches/0.9.0/core/src/main/java/org/imirsel/nema/model/MirexSubmission.java
+++ diy/branches/0.9.0/core/src/main/java/org/imirsel/nema/model/MirexSubmission.java
@@ -31,7 +31,7 @@ import org.hibernate.annotations.GenerationTime;
  * Submission record from the Mirex  submission system
  * 
  * @author gzhu1
- * 
+ * @since 0.7.0
  */
 @Entity
 @Table(name="mirex_submission")
