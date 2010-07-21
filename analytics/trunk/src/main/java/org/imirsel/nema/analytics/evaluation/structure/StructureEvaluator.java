7db274410a1055f5be817a8650802dc20a94138a
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-07-21 06:43:58 +0000

    really stupid bug matlab functions cannot have "-" in them

--- analytics/trunk/src/main/java/org/imirsel/nema/analytics/evaluation/structure/StructureEvaluator.java
+++ analytics/trunk/src/main/java/org/imirsel/nema/analytics/evaluation/structure/StructureEvaluator.java
@@ -219,9 +219,9 @@ public class StructureEvaluator extends EvaluatorImpl {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		String functionID = UUID.nameUUIDFromBytes(jobIdBytes).toString();
+		//String functionID = UUID.nameUUIDFromBytes(jobIdBytes).toString();
 		
-		String evalFunction = "evaluateJob" + functionID + "Set" + testSet.getId();
+		String evalFunction = "evaluateJobSet" + testSet.getId();
 		File evalMFile = new File(evalTempDir.getAbsolutePath() + File.separator + evalFunction + ".m");
 		
 		//write out m file
