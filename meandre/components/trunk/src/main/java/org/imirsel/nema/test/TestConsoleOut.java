1c423d8e6c23d1c5751b8ab09e330331ead39919
kumaramit01 <kumaramit01@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-09-01 22:09:20 +0000

    testing log statements

--- meandre/components/trunk/src/main/java/org/imirsel/nema/test/TestConsoleOut.java
+++ meandre/components/trunk/src/main/java/org/imirsel/nema/test/TestConsoleOut.java
@@ -1,8 +1,5 @@
 package org.imirsel.nema.test;
 
-
-
-
 import java.io.PrintStream;
 import java.util.Date;
 
@@ -12,58 +9,46 @@ import org.meandre.core.ComponentContextException;
 import org.meandre.core.ComponentContextProperties;
 import org.meandre.core.ComponentExecutionException;
 import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
 import org.meandre.annotations.ComponentOutput;
 import org.meandre.annotations.ComponentProperty;
 
-@Component(creator = "Mert Bay", 
-		description = "Test console output", 
-		name = "TestConsoleOut", 
-		tags = "test, console, output", 
-		resources = "",
-		firingPolicy = Component.FiringPolicy.all)
-		
+@Component(creator = "Mert Bay", description = "Test console output", name = "TestConsoleOut", tags = "test, console, output", resources = "", firingPolicy = Component.FiringPolicy.all)
 public class TestConsoleOut extends NemaComponent {
+	
+	@ComponentInput(description = "the message", name = "message")
+	final static String DATA_INPUT_1  ="message";
 
+	@ComponentProperty(defaultValue = "10", description = "Number of times to display the text", name = "Number of Times to Display Text")
+	final static String DATA_PROPERTY_2 = "Number of Times to Display Text";
 
-    @ComponentProperty(defaultValue="Hello World !",
-                      description="text to be displayed on console",
-                      name="Text to Display")
-    final static String DATA_PROPERTY_1 = "Text to Display";
+	@ComponentProperty(defaultValue = "100", description = "wait time between message in miliseconds", name = "Wait Time in miliseconds Between Text Displays")
+	final static String DATA_PROPERTY_3 = "Wait Time in miliseconds Between Text Displays";
 
-    @ComponentProperty(defaultValue="10",
-                      description="Number of times to display the text",
-                      name="Number of Times to Display Text")
-    final static String DATA_PROPERTY_2 = "Number of Times to Display Text";
-    
-    @ComponentProperty(defaultValue="100",
-            description="wait time between message in miliseconds",
-            name="Wait Time in miliseconds Between Text Displays")
-            final static String DATA_PROPERTY_3 = "Wait Time in miliseconds Between Text Displays";
-   
-    @ComponentOutput(description = "output message at the end of the component push", name = "output message")
-    final static String DATA_OUTPUT_1 = "output message";
-   
-  
-    public void initialize(ComponentContextProperties ccp) throws ComponentContextException, ComponentExecutionException{
-    	super.initialize(ccp);
-    }
-    
-  
-    public void dispose(ComponentContextProperties ccp) throws ComponentContextException {
+	@ComponentOutput(description = "output message at the end of the component push", name = "output message")
+	final static String DATA_OUTPUT_1 = "output message";
+
+	public void initialize(ComponentContextProperties ccp)
+			throws ComponentContextException, ComponentExecutionException {
+		super.initialize(ccp);
+	}
+
+	public void dispose(ComponentContextProperties ccp)
+			throws ComponentContextException {
 		super.dispose(ccp);
 	}
-	
+
 	public void execute(ComponentContext ccp)
 			throws ComponentExecutionException, ComponentContextException {
-	  String text = String.valueOf(ccp.getProperty(DATA_PROPERTY_1));	
-        int number = Integer.valueOf(ccp.getProperty(DATA_PROPERTY_2));
-        int wtime = Integer.valueOf(ccp.getProperty(DATA_PROPERTY_3));
-        PrintStream ps=ccp.getOutputConsole();
-    	getLogger().info("Here is the text output: ");
-		for (int i=0;i<number;i++){
-			getLogger().info(i + ":\t"+ text+"\n");
+		String text = (String) ccp.getDataComponentFromInput(DATA_INPUT_1);
+		int number = Integer.valueOf(ccp.getProperty(DATA_PROPERTY_2));
+		int wtime = Integer.valueOf(ccp.getProperty(DATA_PROPERTY_3));
+		PrintStream ps = ccp.getOutputConsole();
+		getLogger().info("Here is the text output: ");
+		for (int i = 0; i < number; i++) {
 			// print on the console
-			ps.print("%%%%"+new Date(System.currentTimeMillis())+" printing to console: "+ text+"\n");
+			ps.print("%%%%" + new Date(System.currentTimeMillis())
+					+ " printing to console: " + text + "\n");
 			ps.flush();
 			try {
 				Thread.currentThread().sleep(wtime);
@@ -72,9 +57,10 @@ public class TestConsoleOut extends NemaComponent {
 				e.printStackTrace();
 			}
 		}
-		ccp.pushDataComponentToOutput(DATA_OUTPUT_1, "DONE: " + this.hashCode());
+		ccp
+				.pushDataComponentToOutput(DATA_OUTPUT_1, "DONE: "
+						+ this.hashCode());
 		getLogger().info("--exit--");
 	}
 
 }
-	
