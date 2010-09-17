5729546dc811c1add7430e6ae74087a79cd13396
zggame <zggame@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2010-09-17 21:15:54 +0000

    adding further code fore row callback in meandre console log reading

--- diy/trunk/webapp/src/main/java/org/imirsel/nema/meandre/util/MeandreConsoleDao.java
+++ diy/trunk/webapp/src/main/java/org/imirsel/nema/meandre/util/MeandreConsoleDao.java
@@ -3,9 +3,11 @@ package org.imirsel.nema.meandre.util;
 import java.util.List;
 
 import org.imirsel.nema.model.Job;
+import org.springframework.jdbc.core.RowCallbackHandler;
 
 public interface MeandreConsoleDao {
     void clearConsole(Job job);
     List<ConsoleEntry> getEntry(Job job);
     List<ConsoleEntry> getEntry(Job job,int maxCount);
+    void handleEntry(Job job,RowCallbackHandler handler);
 }
