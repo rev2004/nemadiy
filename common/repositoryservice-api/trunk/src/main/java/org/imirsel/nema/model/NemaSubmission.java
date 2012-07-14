66125532172714ef1bc46aabbbf0e04d1ef0f57d
afe405 <afe405@cc4868ba-f9d9-11de-9656-bb0dda18cfc1>
2012-07-14 19:30:47 +0000

    added a new constructor for nemasubmissions that creates the objects based off of properties files

--- common/repositoryservice-api/trunk/src/main/java/org/imirsel/nema/model/NemaSubmission.java
+++ common/repositoryservice-api/trunk/src/main/java/org/imirsel/nema/model/NemaSubmission.java
@@ -1,13 +1,26 @@
 package org.imirsel.nema.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 
 public class NemaSubmission {
+	
+	public static final String SUBMISSION_CODE = "submission_code";
+    public static final String SUBMISSION_NAME = "submission_name";
+    public static final String CONTRIBUTORS = "contributors";
+    public static final String ABSTRACT_URL = "abstract_url";
+    
 	private String submissionCode;
 	private String submissionName;
 	private List<NemaContributor> contributors;
 	private String abstractUrl;
+	private Properties props;
 	
 	public NemaSubmission(String submissionCode, String submissionName,
 			List<NemaContributor> contributors, String abstractUrl) {
@@ -15,6 +28,30 @@ public class NemaSubmission {
 		this.submissionName = submissionName;
 		this.contributors = contributors;
 		this.abstractUrl = abstractUrl;
+	}
+	
+	public NemaSubmission(File propsFile) {
+		props = new Properties();        
+        try {
+            InputStream iStream = new FileInputStream(propsFile);
+            if(iStream != null) {
+                props.load(iStream);
+            }else {
+               throw new RuntimeException("File " + propsFile.getAbsolutePath() + " not found");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        this.submissionCode = getProperty(SUBMISSION_CODE);
+        this.submissionName = getProperty(SUBMISSION_NAME);
+        this.abstractUrl = getProperty(ABSTRACT_URL);
+        String contribs = getProperty(CONTRIBUTORS);
+        String[] comps = contribs.split("|");
+        List<NemaContributor> contribList = new ArrayList<NemaContributor>(comps.length);
+        for (int i = 0; i < comps.length; i++) {
+    		contribList.add(NemaContributor.fromString(comps[i]));
+		}
+        this.contributors = contribList;
 	}
 	
 	public String getSubmissionCode() {
@@ -42,5 +79,9 @@ public class NemaSubmission {
 		this.abstractUrl = abstractUrl;
 	}
 	
+	private String getProperty(String name){
+    	String val = props.getProperty(name);
+    	return val;
+    }	
 	
 }
